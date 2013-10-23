68e4d82490deed82a4b9cca6ac5140f214221739
dadhi <none@none>
2013-10-23 15:38:46 +0300

    added: All-in-one AvlTree to check its performance against HashTree.

---
+++ DryIoc.UnitTests/Playground/AvlTree.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DryIoc.UnitTests.Playground
+{
+    public sealed class AvlTree<K, V>
+    {
+        public static readonly AvlTree<K, V> Empty = new AvlTree<K, V>();
+        public bool IsEmpty { get { return Height == 0; } }
+
+        public readonly int Hash;
+        public readonly K Key;
+        public readonly V Value;
+        public readonly KV<K, V>[] Conflicts;
+        public readonly int Height;
+        public readonly AvlTree<K, V> Left, Right;
+
+        public delegate V UpdateValue(V existing, V added);
+
+        public AvlTree<K, V> AddOrUpdate(K key, V value, UpdateValue updateValue = null)
+        {
+            return AddOrUpdate(key.GetHashCode(), key, value, updateValue ?? ReplaceValue);
+        }
+
+        public V GetValueOrDefault(K key, V defaultValue = default(V))
+        {
+            var hash = key.GetHashCode();
+            for (var node = this; node.Height != 0; node = hash < node.Hash ? node.Left : node.Right)
+                if (hash == node.Hash)
+                    return ReferenceEquals(key, node.Key) || key.Equals(node.Key) ? node.Value : node.GetConflictedOrDefault(key, defaultValue);
+            return defaultValue;
+        }
+
+        private V GetConflictedOrDefault(K key, V defaultValue)
+        {
+            if (Conflicts != null)
+                for (var i = 0; i < Conflicts.Length; i++)
+                    if (Equals(Conflicts[i].Key, key))
+                        return Conflicts[i].Value;
+            return defaultValue;
+        }
+
+        /// <summary>Depth-first in-order traversal as described in http://en.wikipedia.org/wiki/Tree_traversal
+        /// The only difference is using fixed size array instead of stack for speed-up (~20% faster than stack).</summary>
+        public IEnumerable<KV<K, V>> TraverseInOrder()
+        {
+            var parents = new AvlTree<K, V>[Height];
+            var parentCount = -1;
+            var node = this;
+            while (!node.IsEmpty || parentCount != -1)
+            {
+                if (!node.IsEmpty)
+                {
+                    parents[++parentCount] = node;
+                    node = node.Left;
+                }
+                else
+                {
+                    node = parents[parentCount--];
+                    yield return new KV<K, V>(node.Key, node.Value);
+                    if (node.Conflicts != null)
+                        for (var i = 0; i < node.Conflicts.Length; i++)
+                            yield return node.Conflicts[i];
+                    node = node.Right;
+                }
+            }
+        }
+
+        #region Implementation
+
+        private AvlTree() { }
+
+        private AvlTree(int hash, K key, V value, KV<K,V>[] conficts, AvlTree<K, V> left, AvlTree<K, V> right)
+        {
+            Hash = hash;
+            Key = key;
+            Value = value;
+            Conflicts = conficts;
+            Left = left;
+            Right = right;
+            Height = 1 + (left.Height > right.Height ? left.Height : right.Height);
+        }
+
+        private static V ReplaceValue(V _, V added)
+        {
+            return added;
+        }
+
+        private AvlTree<K, V> AddOrUpdate(int hash, K key, V value, UpdateValue updateValue)
+        {
+            return Height == 0 ? new AvlTree<K, V>(hash, key, value, null, Empty, Empty)
+                : (hash == Hash ? ResolveConflicts(key, value, updateValue)
+                : (hash < Hash
+                    ? With(Left.AddOrUpdate(hash, key, value, updateValue), Right)
+                    : With(Left, Right.AddOrUpdate(hash, key, value, updateValue)))
+                        .EnsureBalanced());
+        }
+
+        private AvlTree<K, V> ResolveConflicts(K key, V value, UpdateValue updateValue)
+        {
+            if (ReferenceEquals(Key, key) || Key.Equals(key))
+                return new AvlTree<K, V>(Hash, key, updateValue(Value, value), Conflicts, Left, Right);
+
+            if (Conflicts == null)
+                return new AvlTree<K, V>(Hash, Key, Value, new[] { new KV<K, V>(key, value) }, Left, Right);
+
+            var i = Conflicts.Length - 1;
+            while (i >= 0 && !Equals(Conflicts[i].Key, Key)) i--;
+            var conflicts = new KV<K, V>[i != -1 ? Conflicts.Length : Conflicts.Length + 1];
+            Array.Copy(Conflicts, 0, conflicts, 0, Conflicts.Length);
+            conflicts[i != -1 ? i : Conflicts.Length] = new KV<K, V>(key, i != -1 ? updateValue(Conflicts[i].Value, value) : value);
+            return new AvlTree<K, V>(Hash, Key, Value, conflicts, Left, Right);
+        }
+
+        private AvlTree<K, V> EnsureBalanced()
+        {
+            var delta = Left.Height - Right.Height;
+            return delta >= 2 ? With(Left.Right.Height - Left.Left.Height == 1 ? Left.RotateLeft() : Left, Right).RotateRight()
+                : (delta <= -2 ? With(Left, Right.Left.Height - Right.Right.Height == 1 ? Right.RotateRight() : Right).RotateLeft()
+                : this);
+        }
+
+        private AvlTree<K, V> RotateRight()
+        {
+            return Left.With(Left.Left, With(Left.Right, Right));
+        }
+
+        private AvlTree<K, V> RotateLeft()
+        {
+            return Right.With(With(Left, Right.Left), Right.Right);
+        }
+
+        private AvlTree<K, V> With(AvlTree<K, V> left, AvlTree<K, V> right)
+        {
+            return new AvlTree<K, V>(Hash, Key, Value, Conflicts, left, right);
+        }
+
+        #endregion
+    }
+}
