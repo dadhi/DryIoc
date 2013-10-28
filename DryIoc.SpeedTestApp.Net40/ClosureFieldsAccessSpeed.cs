30e9809472bc27e1d5a2c5ce7840de7d45ef9f75
dadhi <none@none>
2013-10-28 11:09:05 +0300

    added: RenameTost. added: Expression refactoring experiment based ExpressionVisitor.

--- DryIoc.SpeedTestApp.Net40/ClosureFieldsAccessSpeed.cs
+++ DryIoc.SpeedTestApp.Net40/ClosureFieldsAccessSpeed.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
+using NUnit.Framework;
 
 namespace DryIoc.SpeedTestApp.Net40
 {
-    public static class ClosureFieldsAccessSpeed
+    [TestFixture]
+    public class ClosureFieldsAccessSpeed
     {
         public static void TestExpr()
         {
-            var one = CreateOne();
+            var one = Expression.Lambda<Func<X>>(CreateOne(), null).Compile();
             var two = CreateTwo();
+            var twoFromOne = Expression.Lambda<Func<X>>(CreateTwoFromOne(), null).Compile();
 
             var times = 1 * 1000 * 1000;
             Console.WriteLine("Testing {0:n0} times in for cycle: ", times);
@@ -34,28 +39,36 @@ namespace DryIoc.SpeedTestApp.Net40
             timer.Stop();
             Console.WriteLine("One took {0} milliseconds to complete.", timer.ElapsedMilliseconds);
 
+            GC.Collect();
+            timer = Stopwatch.StartNew();
+            for (var t = 0; t < times; t++)
+            {
+                x = twoFromOne();
+            }
+            timer.Stop();
+            Console.WriteLine("TwoFromOne took {0} milliseconds to complete.", timer.ElapsedMilliseconds);
+
             GC.KeepAlive(x);
         }
 
-        private static Func<X> CreateOne()
+        private static Expression CreateOne()
         {
             var a = new A();
             var b = new B();
             var c = new C();
-        
+
             var aExpr = Expression.Constant(a);
             var bExpr = Expression.Constant(b);
             var cExpr = Expression.Constant(c);
 
-            var lambda = Expression.Lambda<Func<X>>(
-                Expression.New(typeof (X).GetConstructors()[0],
+            var body =
+                Expression.New(typeof(X).GetConstructors()[0],
                     aExpr, bExpr, cExpr,
 
-                    Expression.New(typeof (A1).GetConstructors()[0], aExpr),
-                    Expression.New(typeof (B1).GetConstructors()[0], bExpr),
-                    Expression.New(typeof (C1).GetConstructors()[0], cExpr)),
-                null);
-            return lambda.Compile();
+                    Expression.New(typeof(A1).GetConstructors()[0], aExpr),
+                    Expression.New(typeof(B1).GetConstructors()[0], bExpr),
+                    Expression.New(typeof(C1).GetConstructors()[0], cExpr));
+            return body;
         }
 
         private static Func<X> CreateTwo()
@@ -69,21 +82,53 @@ namespace DryIoc.SpeedTestApp.Net40
             var cExpr = Expression.Variable(typeof(C));
 
             var block = Expression.Block(
-                new[] {aExpr, bExpr, cExpr},
+                new[] { aExpr, bExpr, cExpr },
 
                 Expression.Assign(aExpr, Expression.Constant(a)),
                 Expression.Assign(bExpr, Expression.Constant(b)),
                 Expression.Assign(cExpr, Expression.Constant(c)),
 
-                Expression.New(typeof(X).GetConstructors()[0], 
+                Expression.New(typeof(X).GetConstructors()[0],
                     aExpr, bExpr, cExpr,
-                    
+
                     Expression.New(typeof(A1).GetConstructors()[0], aExpr),
                     Expression.New(typeof(B1).GetConstructors()[0], bExpr),
                     Expression.New(typeof(C1).GetConstructors()[0], cExpr)));
 
             var lambda = Expression.Lambda<Func<X>>(block, null);
             return lambda.Compile();
+        }
+
+        [Test]
+        public static Expression CreateTwoFromOne()
+        {
+            var funcExpr = CreateOne();
+            var visitor = new MoveConstantsToVariableVisitor();
+            var funcExprRefactored = visitor.Visit(funcExpr);
+            if (visitor.Vars.Count == 0)
+                return funcExpr;
+
+            var block = Expression.Block(
+                visitor.Vars.Values,
+                visitor.Vars.Select(item => Expression.Assign(item.Value, item.Key)).Concat(
+                new[] { funcExprRefactored }));
+
+            return block;
+        }
+
+    }
+
+    class MoveConstantsToVariableVisitor : ExpressionVisitor
+    {
+        public Dictionary<ConstantExpression, ParameterExpression>
+            Vars = new Dictionary<ConstantExpression, ParameterExpression>();
+
+        protected override Expression VisitConstant(ConstantExpression constExpr)
+        {
+            ParameterExpression varExpr;
+            if (!Vars.TryGetValue(constExpr, out varExpr))
+                Vars.Add(constExpr, varExpr = Expression.Variable(constExpr.Type));
+            return varExpr;
         }
     }
 
---
+++ RenameProjectSteps.md
@@ -0,0 +1,8 @@
+## Rename project steps:
+
+1. Rename project folder
+2. Rename .proj file and other tools ProjectName. files
+4. Rename ProjectName inside .proj file: RootNamespace and AssemblyName 
+3. Rename ProjectName in .sln file
+
+TADAH!
