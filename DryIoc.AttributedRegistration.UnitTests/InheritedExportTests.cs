f5b72c283b2a039f7717cd5b3d8d2d74065798ea
dadhi <none@none>
2013-11-11 15:26:50 +0300

    fixed: Added missing file InheritedExportTests.

---
+++ DryIoc.AttributedRegistration.UnitTests/InheritedExportTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace DryIoc.AttributedRegistration.UnitTests
+{
+    [TestFixture]
+    public class InheritedExportTests
+    {
+        [Test]
+        public void It_is_possible_to_mark_interface_to_export_all_its_implementations()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void It_is_possible_to_mark_abstract_class_to_export_all_its_implementations()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void It_is_possible_to_mark_class_as_not_discoverable()
+        {
+            Assert.Fail();
+        }
+    }
+}
