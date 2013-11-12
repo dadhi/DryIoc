67cfcbda42c006f2f2755b786e00845a36e9a745
dadhi <none@none>
2013-11-12 21:00:49 +0300

    added: CodeGeneration/ToCode test.

---
+++ DryIoc.AttributedRegistration.UnitTests/CodeGenerationTests.cs
@@ -0,0 +1,28 @@
+﻿using DryIoc.AttributedRegistration.UnitTests.CUT;
+using NUnit.Framework;
+
+namespace DryIoc.AttributedRegistration.UnitTests
+{
+    [TestFixture]
+    public class CodeGenerationTests
+    {
+        [Test]
+        public void Should_properly_print_registration_info()
+        {
+            var info = AttributedRegistrator.GetRegistrationInfoOrDefault(typeof(ForExportBaseImpl));
+
+            var code = info.ToCode();
+
+            Assert.That(code, Is.EqualTo(
+@"new RegistrationInfo {
+    ImplementationType = typeof(DryIoc.AttributedRegistration.UnitTests.CUT.ForExportBaseImpl),
+    Exports = new[] {
+        new ExportInfo { ServiceType = typeof(DryIoc.AttributedRegistration.UnitTests.CUT.ForExportBase), ServiceName = null },
+    },
+    IsSingleton = true,
+    MetadataAttributeIndex = -1,
+    FactoryType = DryIoc.FactoryType.Service
+}"));
+        }
+    }
+}
