579f1e45a32016e987368ddaa4e4a3f9f5900a93
dadhi <none@none>
2013-11-26 13:04:41 +0300

    fixed: RenameProject.ps1 scipt. Now processing AssemblyInfo if specified.

--- Potter/Properties/AssemblyInfo.cs
+++ Potter/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RenameTest")]
+[assembly: AssemblyTitle("Potter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RenameTest")]
+[assembly: AssemblyProduct("Potter")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
