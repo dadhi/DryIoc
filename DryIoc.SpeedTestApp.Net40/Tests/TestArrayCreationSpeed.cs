2cdff4993d44290e9bf0f39441d1fb9110232ff8
dadhi <none@none>
2013-12-04 13:54:35 +0300

    added: Fast Chained/Enumerator with Enumerable.Count/creation speed on par with yield return.

--- DryIoc.SpeedTestApp.Net40/Tests/TestArrayCreationSpeed.cs
+++ DryIoc.SpeedTestApp.Net40/Tests/TestArrayCreationSpeed.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -30,7 +31,7 @@ namespace DryIoc.SpeedTestApp.Net40.Tests
             }
             timer.Stop();
             Console.WriteLine("Array initializer took {0} milliseconds to complete.", timer.ElapsedMilliseconds);
-            
+
             GC.Collect();
             timer = Stopwatch.StartNew();
             for (int t = 0; t < times; t++)
@@ -38,9 +39,25 @@ namespace DryIoc.SpeedTestApp.Net40.Tests
                 result = new ItemsConsumer(GetItems());
             }
             timer.Stop();
-            Console.WriteLine("Lazy Enumerable took {0} milliseconds to complete.", timer.ElapsedMilliseconds);            
-            
+            Console.WriteLine("Lazy Enumerable took {0} milliseconds to complete.", timer.ElapsedMilliseconds);
+
+            GC.Collect();
+            timer = Stopwatch.StartNew();
+            for (int t = 0; t < times; t++)
+            {
+                result = new ItemsConsumer(new Chained<IItem>(
+                    new SomeItem(), new Chained<IItem>(
+                    new AnotherItem(), new Chained<IItem>(
+                    new YetAnotherItem(), new Chained<IItem>(
+                    new YetAnotherItem(), new Chained<IItem>(
+                    new YetAnotherItem()))))));
+            }
+            timer.Stop();
+            Console.WriteLine("Chained took {0} milliseconds to complete.", timer.ElapsedMilliseconds);
+
             GC.KeepAlive(result);
+
+            var items = new List<IItem>();
         }
 
         private static IEnumerable<IItem> GetItems()
@@ -50,6 +67,60 @@ namespace DryIoc.SpeedTestApp.Net40.Tests
             yield return new YetAnotherItem();
             yield return new YetAnotherItem();
             yield return new YetAnotherItem();
+        }
+    }
+
+    public sealed class Chained<T> : IEnumerable<T>
+    {
+        public readonly T Value;
+        public readonly Chained<T> Next;
+
+        public Chained(T value, Chained<T> next = null)
+        {
+            Value = value;
+            Next = next;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ChainedEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private sealed class ChainedEnumerator : IEnumerator<T>
+        {
+            private Chained<T> _current;
+
+            public ChainedEnumerator(Chained<T> source)
+            {
+                _current = new Chained<T>(default(T), source);
+            }
+
+            public T Current
+            {
+                get { return _current.Value; }
+            }
+
+            public bool MoveNext()
+            {
+                return (_current = _current.Next) == null;
+            }
+
+            public void Dispose() { }
+
+            public void Reset()
+            {
+                throw new NotSupportedException();
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
         }
     }
 
---
+++ DryIoc.UnitTests/Playground/CreateEnumerableWithExpressionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace DryIoc.UnitTests.Playground
+{
+    [TestFixture]
+    public class CreateEnumerableWithExpressionTests
+    {
+        [Test]
+        public void Can_create_method_yielding_items()
+        {
+            Expression<Func<IEnumerable<IItem>>> getItems = () => GetItems();
+
+            //Func<Expression[], Expression<Func<IEnumerable<IItem>>>> getItems2 = items =>
+            //{
+                 
+            //};
+        }
+
+        IEnumerable<IItem> GetItems()
+        {
+            yield return new SomeItem();
+            yield return new AnotherItem();
+        }
+
+        IEnumerable<IItem> ResolveItems()
+        {
+            //yield return ResolveItem();
+            //yield return ResolveNextItem();
+            return null;
+        }
+    }
+
+    internal class SomeItem : IItem {}
+    internal class AnotherItem : IItem {}
+
+    internal interface IItem {}
+}
