c04c9e37b6b2e45a8176f28ca6cb849bc0ff1891
dadhi <none@none>
2013-12-12 18:00:03 +0300

    renamed: DI.MefAttrModel CompileTime tests project.

---
+++ DryIoc.MefAttributedModel.CompileTimeAssemblyScan.Tests/CompileTimeRegistrationTests.cs
@@ -0,0 +1,92 @@
+﻿using DryIoc.AttributedRegistration.UnitTests.CUT;
+using DryIoc.MefAttributedModel;
+using NUnit.Framework;
+
+namespace DryIoc.AttributedRegistration.CompileTimeAssemblyScan.Tests
+{
+    [TestFixture]
+    public class CompileTimeRegistrationTests
+    {
+        [Test]
+        public void Can_register_service_with_constants_alone()
+        {
+            var container = new Container(AttributedModel.DefaultSetup);
+            container.RegisterExports(new[]
+            {
+                new TypeExportInfo
+                {
+                    Type = typeof(AnotherService),
+                    Exports = new[] { 
+                        new ExportInfo { ServiceType = typeof(IService), ServiceName = "another" }
+                    },
+                    IsSingleton = true,
+                    MetadataAttributeIndex = -1,
+                }
+            });
+
+            var service = container.Resolve<IService>("another");
+
+            Assert.NotNull(service);
+        }
+
+        [Test]
+        public void Can_register_decorator_and_wrapper_with_constants_alone()
+        {
+            var container = new Container(AttributedModel.DefaultSetup);
+            container.RegisterExports(new[]
+            {
+                new TypeExportInfo
+                {
+                    Type = typeof(Service),
+                    Exports = new[] {
+                        new ExportInfo { ServiceType = typeof(IService), ServiceName = "some" } },
+                    IsSingleton = true,
+                    MetadataAttributeIndex = -1
+                },
+
+                new TypeExportInfo
+                {
+                    Type = typeof(AnotherService),
+                    Exports = new[] {
+                        new ExportInfo { ServiceType = typeof(IService), ServiceName = null } },
+                    IsSingleton = false,
+                    MetadataAttributeIndex = -1,
+                    FactoryType = FactoryType.Decorator,
+                },
+
+                new TypeExportInfo
+                {
+                    Type = typeof(Wrap<>),
+                    Exports = new[] {
+                        new ExportInfo { ServiceType = typeof(Wrap<>), ServiceName = null } }, IsSingleton = false,
+                    MetadataAttributeIndex = -1,
+                    FactoryType = FactoryType.GenericWrapper
+                },
+            });
+
+            var wrapped = container.Resolve<Wrap<IService>>("some");
+
+            Assert.That(wrapped.Value, Is.InstanceOf<AnotherService>());
+        }
+
+        //[Test]
+        //public void Can_use_compile_time_generated_registrations()
+        //{
+        //    var container = new Container(AttributedRegistrator.DefaultSetup);
+        //    container.RegisterExported(CompileTimeGeneratedRegistrator.Registrations);
+
+        //    Assert.DoesNotThrow(() =>
+        //        container.Resolve<Meta<Func<IServiceWithMetadata>, IViewMetadata>[]>());
+        //}
+    }
+
+    public class Wrap<T>
+    {
+        public T Value { get; set; }
+
+        public Wrap(T value)
+        {
+            Value = value;
+        }
+    }
+}
--- DryIoc.MefAttributedModel.CompileTimeAssemblyScan.Tests/Properties/AssemblyInfo.cs
+++ DryIoc.MefAttributedModel.CompileTimeAssemblyScan.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DryIoc.AttributedRegistration.UnitTests")]
+[assembly: AssemblyTitle("DryIoc.MefAttributedModel.CompileTimeAssemblyScan.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DryIoc.AttributedRegistration.UnitTests")]
+[assembly: AssemblyProduct("DryIoc.MefAttributedModel.CompileTimeAssemblyScan.Tests")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("da90d985-d68a-4289-b85a-6c0ae7ecb519")]
+[assembly: Guid("7cc14938-408c-448d-a5c2-da55a3c635ca")]
 
 // Version information for an assembly consists of the following four values:
 //
