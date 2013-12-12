30963da1d8b0a07fb88249e83e289f6a09d365b9
dadhi <none@none>
2013-12-12 18:08:36 +0300

    renamed: Mef tests CUT

--- DryIoc.MefAttributedModel.UnitTests.CUT/Properties/AssemblyInfo.cs
+++ DryIoc.MefAttributedModel.UnitTests.CUT/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DryIoc.AttributedRegistration.CompileTimeAssemblyScan.Tests")]
+[assembly: AssemblyTitle("DryIoc.MefAttributedModel.UnitTests.CUT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DryIoc.AttributedRegistration.CompileTimeAssemblyScan.Tests")]
+[assembly: AssemblyProduct("DryIoc.MefAttributedModel.UnitTests.CUT")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7cc14938-408c-448d-a5c2-da55a3c635ca")]
+[assembly: Guid("c88420bf-cb61-4c79-8436-cae94167d74a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("DryIoc.AttributedRegistration.CompileTimeAssemblyScan.Tests")]
