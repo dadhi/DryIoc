a054e89811983561ef6e5e4c75584baaebfdea2d
dadhi <none@none>
2013-12-24 15:08:13 +0300

    changed: Version to 1.0.10. renamed: DryIoc.Code to just DryIoc, to make source files option default. renamed: DryIoc to DryIoc dll nuspec.

--- nuget/BuildPackage.bat
+++ nuget/BuildPackage.bat
@@ -1,3 +1,3 @@
 md ..\bin\nuget
 call nuget pack DryIoc.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
-call nuget pack DryIoc.Code.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
+call nuget pack DryIoc.dll.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
--- nuget/PushPackage.bat
+++ nuget/PushPackage.bat
@@ -1,4 +1,4 @@
 set /p apikey= < apikey.txt
 set version=%1
 call nuget push ..\bin\nuget\DryIoc.%version%.nupkg -Source https://staging.nuget.org -ApiKey %apikey%
-call nuget push ..\bin\nuget\DryIoc.Code.%version%.nupkg -Source https://staging.nuget.org -ApiKey %apikey%
+call nuget push ..\bin\nuget\DryIoc.dll.%version%.nupkg -Source https://staging.nuget.org -ApiKey %apikey%
