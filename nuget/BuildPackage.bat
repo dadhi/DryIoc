ff18f76b7f8314208d7328ffbc8b28a3159e7044
dadhi <none@none>
2013-12-26 13:10:15 +0300

    added: DryIoc.MefAttributedModel nuspecs. changed: Updated nuget scripts.

--- nuget/BuildPackage.bat
+++ nuget/BuildPackage.bat
@@ -1,3 +1,7 @@
 md ..\bin\nuget
+
 call nuget pack DryIoc.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
 call nuget pack DryIoc.dll.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
+
+call nuget pack DryIoc.MefAttributedModel.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
+call nuget pack DryIoc.MefAttributedModel.dll.nuspec -NonInteractive -OutputDirectory ..\bin\nuget
---
+++ nuget/readme.txt
@@ -0,0 +1,3 @@
+Batch scripts prereqs:
+- Make sure that path to NuGet.exe is listed in Environment path variable.
+- Create apikey.txt with your API Key and put it here in Batch scripts folder.   
