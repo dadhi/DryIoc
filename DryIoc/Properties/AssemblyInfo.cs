a8f8ec7146cb52911472d31cfef23a4445de1d39
dadhi <none@none>
2014-01-09 17:28:35 +0300

    changed: Version to 1.2.0

--- DryIoc/Properties/AssemblyInfo.cs
+++ DryIoc/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("6f2bee9b-8e7f-4806-97b7-4b63f2f0cde0")]
