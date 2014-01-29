b6b3bc0dc6443cac93c18306e176a48933645653
dadhi <none@none>
2014-01-29 10:45:36 +0300

    fixed: Proj reference to old file. changed: Finished Get and Add on HashTrie.
    --HG--
    branch : dev

--- DryIoc.Playground/HashArrayMappedTrieTests.cs
+++ DryIoc.Playground/HashArrayMappedTrieTests.cs
@@ -8,21 +8,16 @@ namespace DryIoc.Playground
     public class HashArrayMappedTrieTests
     {
         [Test]
-        public void Test_how_int_hash_code_is_working_in_edge_cases()
+        public void Test_how_int_hash_code_is_working_at_edge_cases()
         {
             // Hash code for negative is still negative and equal to key
-            var x = -7;
-            Assert.AreEqual(-7, x.GetHashCode());
-
-            // Conversation of negative to UINT does not throw.
-            var ux = (uint)x;
-            var uux = unchecked((uint)x);
+            Assert.AreEqual(-7, -7.GetHashCode());
         }
 
         [Test]
         public void Create_trie_and_add_value_to_it()
         {
-            var trie = Hamt<string>.Empty;
+            var trie = HashTrie<string>.Empty;
             trie = trie.AddOrUpdate(705, "a");
             trie = trie.AddOrUpdate(706, "b");
             trie = trie.AddOrUpdate(750, "c");
@@ -31,109 +26,139 @@ namespace DryIoc.Playground
             trie = trie.AddOrUpdate(5, "5x");
             trie = trie.AddOrUpdate(555555555, "55x");
             trie = trie.AddOrUpdate(750, "C");
-            
+
+            Assert.AreEqual(null, trie.GetValueOrDefault(13));
             Assert.AreEqual("C", trie.GetValueOrDefault(750));
+            Assert.AreEqual("0x", trie.GetValueOrDefault(0));
+            Assert.AreEqual("A", trie.GetValueOrDefault(705));
+            Assert.AreEqual("55x", trie.GetValueOrDefault(555555555));
+            Assert.AreEqual(null, trie.GetValueOrDefault(-1));
         }
     }
 
-    public sealed class Hamt<V>
+    public sealed class HashTrie<V>
     {
-        public static Hamt<V> Empty = new Hamt<V>();
-
-        private readonly HamtNode<V> _root;
-
-        public Hamt(HamtNode<V> root = null)
-        {
-            _root = root ?? HamtNode<V>.Empty;
-        }
+        public static HashTrie<V> Empty = new HashTrie<V>();
 
         public V GetValueOrDefault(int key, V defaultValue = default (V))
         {
-            var node = _root;
-            var hash = (uint)key;
-            while (hash != 0)
+            return _root.GetValueOrDefault(key, defaultValue);
+        }
+
+        public HashTrie<V> AddOrUpdate(int key, V value)
+        {
+            return new HashTrie<V>(_root.AddOrUpdate(key, value));
+        }
+
+        private readonly Node _root;
+
+        private HashTrie(Node root = null)
+        {
+            _root = root ?? Node.EmptyNode;
+        }
+
+        private sealed class Node
+        {
+            public static readonly Node EmptyNode = new Node();
+
+            public V GetValueOrDefault(int hash, V defaultValue)
             {
+                var index = hash & LEVEL_MASK; // index from 0 to 31
+                var indexBit = (uint)1 << index;
+                if ((_indexBitmap & indexBit) == 0)
+                    return defaultValue;
 
+                // get node or value by looking at real index
+                var pastIndexBitmap = _indexBitmap >> index;
+                var realIndex = pastIndexBitmap == 1 ? _nodes.Length - 1 // the last bit matched, that means index is the last
+                    : _nodes.Length - GetSetBitsCount(pastIndexBitmap);
 
-                hash >>= 5;
+                var valueOrNode = _nodes[realIndex];
+                if (!(valueOrNode is Node))
+                    return (V)valueOrNode;
+
+                var restOfHash = hash >> LEVEL_BITS;
+                return restOfHash == 0 ? defaultValue
+                    : ((Node)valueOrNode).GetValueOrDefault(restOfHash, defaultValue);
             }
 
-            return defaultValue;
-        }
-
-        public Hamt<V> AddOrUpdate(int key, V value)
-        {
-            return new Hamt<V>(_root.AddOrUpdate((uint)key, value));
-        }
-    }
-
-    public class HamtNode<V>
-    {
-        public static readonly HamtNode<V> Empty = new HamtNode<V>();
-
-        public readonly object[] Nodes;   // up to 32 nodes: sub nodes or values.
-        public readonly uint IndexBitmap; // bits indicating what nodes are in use.
-
-
-        public HamtNode(object[] nodes, uint indexBitmap)
-        {
-            Nodes = nodes;
-            IndexBitmap = indexBitmap;
-        }
-
-        public HamtNode<V> AddOrUpdate(uint hash, V value)
-        {
-            var index = (int)(hash & LEVEL_MASK); // index from 0 to 31
-
-            // get value or node
-            var restOfHash = hash >> 5;
-            var valueOrNode = restOfHash == 0 ? (object)value : Empty.AddOrUpdate(restOfHash, value);
-
-            // insert or update node
-            var indexBit = (uint)1 << index;
-            if (Nodes == null)
-                return new HamtNode<V>(new[] { valueOrNode }, indexBit);
-
-            // find real index where to insert into new nodes
-            var pastIndexBitmap = IndexBitmap >> index;
-            var pastIndexNodeCount = GetSetBitsCount(pastIndexBitmap);
-            var realIndex = Nodes.Length - pastIndexNodeCount;
-
-            // insert:
-            if ((IndexBitmap & indexBit) == 0)
+            public Node AddOrUpdate(int hash, V value)
             {
-                // otherwise copy old nodes with extra room for new node
-                var nodesToInsert = new object[Nodes.Length + 1];
+                var index = hash & LEVEL_MASK; // index from 0 to 31
 
-                // Copy up to index, set node to index, and copy past of index nodes.
-                if (realIndex != 0)
-                    Array.Copy(Nodes, 0, nodesToInsert, 0, realIndex);
-                nodesToInsert[realIndex] = valueOrNode;
-                if (realIndex != Nodes.Length)
-                    Array.Copy(Nodes, realIndex, nodesToInsert, realIndex + 1, pastIndexNodeCount);
+                // get value or node
+                var restOfHash = hash >> LEVEL_BITS;
+                var valueOrNode = restOfHash == 0 ? (object)value : EmptyNode.AddOrUpdate(restOfHash, value);
 
-                return new HamtNode<V>(nodesToInsert, IndexBitmap | indexBit);
+                // insert or update node
+                var indexBit = (uint)1 << index;
+                if (_nodes == null)
+                    return new Node(new[] { valueOrNode }, indexBit);
+
+                // find real index where to insert into new nodes
+                var pastIndexBitmap = _indexBitmap >> index;
+                var pastIndexCount = pastIndexBitmap == 0 ? 0 : GetSetBitsCount(pastIndexBitmap);
+                var realIndex = _nodes.Length - pastIndexCount;
+
+                // insert:
+                if ((_indexBitmap & indexBit) == 0)
+                {
+                    // otherwise copy old nodes with extra room for new node
+                    var nodesToInsert = new object[_nodes.Length + 1];
+
+                    // Copy up to index, set node to index, and copy past of index nodes.
+                    if (realIndex != 0)
+                        Array.Copy(_nodes, 0, nodesToInsert, 0, realIndex);
+                    nodesToInsert[realIndex] = valueOrNode;
+                    if (pastIndexCount != 0)
+                        Array.Copy(_nodes, realIndex, nodesToInsert, realIndex + 1, pastIndexCount);
+
+                    return new Node(nodesToInsert, _indexBitmap | indexBit);
+                }
+
+                // update:
+                // copy nodes and replace value at index
+                var nodesToUpdate = new object[_nodes.Length];
+                if (nodesToUpdate.Length > 1)
+                    Array.Copy(_nodes, 0, nodesToUpdate, 0, nodesToUpdate.Length);
+                nodesToUpdate[realIndex] = valueOrNode;
+
+                return new Node(nodesToUpdate, _indexBitmap);
             }
 
-            // update:
-            // copy nodes and replace value at index
-            var nodesToUpdate = new object[Nodes.Length];
-            if (nodesToUpdate.Length > 1)
-                Array.Copy(Nodes, 0, nodesToUpdate, 0, nodesToUpdate.Length);
-            nodesToUpdate[realIndex] = valueOrNode;
+            private const int LEVEL_MASK = 31;  // Hash mask to find hash part on each trie level.
+            private const int LEVEL_BITS = 5;   // Number of bits from hash corresponding to one level.
+            
+            private readonly object[] _nodes;   // Up to 32 nodes: sub nodes or values.
+            private readonly uint _indexBitmap; // Bits indicating nodes at what index are in use.
 
-            return new HamtNode<V>(nodesToUpdate, IndexBitmap);
-        }
+            private Node() { }
 
-        private HamtNode() { }
+            private Node(object[] nodes, uint indexBitmap)
+            {
+                _nodes = nodes;
+                _indexBitmap = indexBitmap;
+            }
 
-        private const uint LEVEL_MASK = 0x1F; // 11111
+            // http://www-graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan
+            private static int GetSetBitsCount(uint i)
+            {
+                var count = 0;
+                while (i != 0)
+                {
+                    i &= i - 1;
+                    ++count;
+                }
+                return count;
+            }
 
-        private static uint GetSetBitsCount(uint i)
-        {
-            i = i - ((i >> 1) & 0x55555555);
-            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-            return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+            // variable-precision SWAR algorithm http://playingwithpointers.com/swar.html
+            //private static int GetSetBitsCountSWAR(uint i)
+            //{
+            //    i = i - ((i >> 1) & 0x55555555);
+            //    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            //    return (int)((((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24);
+            //}
         }
     }
 
