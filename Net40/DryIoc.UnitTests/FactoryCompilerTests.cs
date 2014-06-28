41c5701a52331a6f7560202cd614c45500195dec
dadhi <none@none>
2014-06-28 17:33:06 +0300

    added: FactoryCompiler test.
    --HG--
    branch : edge

---
+++ Net40/DryIoc.UnitTests/FactoryCompilerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace DryIoc.Playground
+{
+    [TestFixture]
+    public class FactoryCompilerTests
+    {
+        [Test]
+        public void Compile_delegate_with_nested_lambda()
+        {
+            Expression<FactoryDelegate> factory = 
+                _ => new Func<Scope, object>(x => x.GetOrAdd(0, () => "a")).Invoke(new Scope());
+
+            var factoryDelegate = factory.Body.CompileToDelegate();
+            var result = (string)factoryDelegate.Invoke(null);
+
+            Assert.That(result, Is.EqualTo("a"));
+        }
+    }
+}
