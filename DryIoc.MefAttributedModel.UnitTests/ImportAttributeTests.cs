3be11a829b282cd864ba6044342eabb689e1638f
dadhi <none@none>
2014-07-20 19:18:35 +0300

    added: Failing test fro Import.ContractType.
    --HG--
    branch : dev

---
+++ DryIoc.MefAttributedModel.UnitTests/ImportAttributeTests.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using DryIoc.MefAttributedModel.UnitTests.CUT;
+using NUnit.Framework;
+
+namespace DryIoc.MefAttributedModel.UnitTests
+{
+    [TestFixture]
+    public class ImportAttributeTests
+    {
+        [Test]
+        public void Inject_service_as_parameter_of_service_implemented_type_using_Import_ContractType()
+        {
+            var container = new Container().WithAttributedModel();
+            container.RegisterExports(typeof(Client), typeof(Service));
+
+            container.Resolve<Client>();
+        }
+
+        [Export]
+        public class Client
+        {
+            public IService Some { get; set; }
+
+            public Client([Import(typeof(Service))]IService service)
+            {
+                Some = service;
+            }
+        }
+
+        [Export]
+        public class Service : IService { }
+    }
+}
