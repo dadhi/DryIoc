6fca73a61fe0a0e286d9c2f473f8f9f4247439a5
dadhi <none@none>
2014-08-22 10:21:01 +0300

    added: 2-3 tree GetValueOrDefault. Optimized memory pressure by merging Item into OneItemLeaf.
    --HG--
    branch : edge

--- DryIoc.Playground/TwoThreeTreeTests.cs
+++ DryIoc.Playground/TwoThreeTreeTests.cs
@@ -22,30 +22,54 @@ namespace DryIoc.Playground
             //       10   30  50 70 90,100
 
             var root = (TwoThreeTree<int, string>.OneItemTree)tree.Root;
-            Assert.That(root.LeftItem.Key, Is.EqualTo(40));
+            Assert.That(root.Item.Key, Is.EqualTo(40));
 
             var rootLeft = (TwoThreeTree<int, string>.OneItemTree)root.Left;
-            Assert.That(rootLeft.LeftItem.Key, Is.EqualTo(20));
+            Assert.That(rootLeft.Item.Key, Is.EqualTo(20));
 
             var rootLeftLeft = (TwoThreeTree<int, string>.OneItemLeaf)rootLeft.Left;
-            Assert.That(rootLeftLeft.LeftItem.Key, Is.EqualTo(10));
+            Assert.That(rootLeftLeft.Key, Is.EqualTo(10));
 
             var rootLeftRight = (TwoThreeTree<int, string>.OneItemLeaf)rootLeft.Right;
-            Assert.That(rootLeftRight.LeftItem.Key, Is.EqualTo(30));
+            Assert.That(rootLeftRight.Key, Is.EqualTo(30));
 
             var rootRight = (TwoThreeTree<int, string>.TwoItemsTree)root.Right;
             Assert.That(rootRight.LeftItem.Key, Is.EqualTo(60));
             Assert.That(rootRight.RightItem.Key, Is.EqualTo(80));
 
             var rootRightLeft = (TwoThreeTree<int, string>.OneItemLeaf)rootRight.Left;
-            Assert.That(rootRightLeft.LeftItem.Key, Is.EqualTo(50));
+            Assert.That(rootRightLeft.Key, Is.EqualTo(50));
 
             var rootRightMiddle = (TwoThreeTree<int, string>.OneItemLeaf)rootRight.Middle;
-            Assert.That(rootRightMiddle.LeftItem.Key, Is.EqualTo(70));
+            Assert.That(rootRightMiddle.Key, Is.EqualTo(70));
 
             var rootRightRight = (TwoThreeTree<int, string>.TwoItemsLeaf)rootRight.Right;
             Assert.That(rootRightRight.LeftItem.Key, Is.EqualTo(90));
             Assert.That(rootRightRight.RightItem.Key, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void Get_stored_value_by_key()
+        {
+            var items = new[] { 50, 60, 70, 40, 30, 20, 10, 80, 90, 100 };
+
+            var tree = TwoThreeTree<int, int>.Empty;
+
+            for (var i = 0; i < items.Length; i++)
+                tree = tree.AddOrUpdate(items[i], i);
+
+            // the result for keys should be:
+            //              40
+            //            /    \
+            //          20     60,80
+            //         /  \    / |   \
+            //       10   30  50 70 90,100
+
+            for (var i = 0; i < items.Length; i++)
+            {
+                var value = tree.GetValueOrDefault(items[i], -1);
+                Assert.That(value, Is.EqualTo(i));
+            }
         }
     }
 
@@ -63,25 +87,21 @@ namespace DryIoc.Playground
 
         public TwoThreeTree<K, V> AddOrUpdate(K key, V value)
         {
-            return IsEmpty 
-                ? new TwoThreeTree<K, V>(new OneItemLeaf(new Item(key.GetHashCode(), key, value)))
-                : new TwoThreeTree<K, V>(Root.AddOrUpdate(new Item(key.GetHashCode(), key, value)));
+            return IsEmpty ? new TwoThreeTree<K, V>(new OneItemLeaf(key.GetHashCode(), key, value)) 
+                : new TwoThreeTree<K, V>(Root.AddOrUpdate(new OneItemLeaf(key.GetHashCode(), key, value)));
         }
 
-        public sealed class Item
+        public V GetValueOrDefault(K key, V defaultValue = default(V))
         {
-            public readonly int Hash;
-            public readonly K Key;
-            public readonly V Value;
-            public readonly KV<K, V>[] Conflicts;
+            if (Root == null) return defaultValue;
+            var node = Root;
+            var hash = key.GetHashCode();
+            while (node.Type != NodeType.OneItemLeaf)
+                node = node.LookFor(hash);
 
-            public Item(int hash, K key, V value, KV<K, V>[] conflicts = null)
-            {
-                Hash = hash;
-                Key = key;
-                Value = value;
-                Conflicts = conflicts;
-            }
+            var item = (OneItemLeaf)node;
+            return hash == item.Hash && (ReferenceEquals(key, item.Key) || key.Equals(item.Key))
+                ? item.Value : item.GetConflictedValueOrDefault(key, defaultValue);
         }
 
         public enum NodeType { OneItemLeaf, TwoItemsLeaf, OneItemTree, TwoItemsTree }
@@ -89,79 +109,106 @@ namespace DryIoc.Playground
         public interface INode
         {
             NodeType Type { get; }
-            INode AddOrUpdate(Item item);
+            INode AddOrUpdate(OneItemLeaf item);
+            INode LookFor(int hash);
         }
 
-        public class OneItemLeaf : INode
+        public sealed class OneItemLeaf : INode
         {
-            public virtual NodeType Type { get { return NodeType.OneItemLeaf; } }
-            public readonly Item LeftItem;
+            public NodeType Type { get { return NodeType.OneItemLeaf; } }
 
-            public OneItemLeaf(Item leftItem)
+            public readonly int Hash;
+            public readonly K Key;
+            public readonly V Value;
+            public readonly KV<K, V>[] Conflicts;
+
+            public OneItemLeaf(int hash, K key, V value, KV<K, V>[] conflicts = null)
             {
-                LeftItem = leftItem;
+                Hash = hash;
+                Key = key;
+                Value = value;
+                Conflicts = conflicts;
             }
 
-            public virtual INode AddOrUpdate(Item item)
+            public INode AddOrUpdate(OneItemLeaf item)
             {
-                return item.Hash > LeftItem.Hash ? new TwoItemsLeaf(LeftItem, item) : new TwoItemsLeaf(item, LeftItem);
+                return item.Hash > Hash ? new TwoItemsLeaf(this, item) : new TwoItemsLeaf(item, this);
+            }
+
+            INode INode.LookFor(int _) { return this; }
+
+            public V GetConflictedValueOrDefault(K key, V defaultValue)
+            {
+                throw new System.NotImplementedException();
             }
         }
 
-        public sealed class OneItemTree : OneItemLeaf
+        public sealed class OneItemTree : INode
         {
-            public override NodeType Type { get { return NodeType.OneItemTree; } }
+            public NodeType Type { get { return NodeType.OneItemTree; } }
+            public readonly OneItemLeaf Item;
             public readonly INode Left, Right;
 
-            public OneItemTree(Item item, INode left, INode right)
-                : base(item)
+            public OneItemTree(OneItemLeaf item, INode left, INode right)
             {
+                Item = item;
                 Left = left;
                 Right = right;
             }
 
-            public override INode AddOrUpdate(Item item)
+            public INode AddOrUpdate(OneItemLeaf item)
             {
-                if (item.Hash < LeftItem.Hash)
+                if (item.Hash < Item.Hash)
                 {
                     var newLeft = Left.AddOrUpdate(item);
-                    if (!IsEmerged(newLeft, Left)) 
-                        return new OneItemTree(LeftItem, newLeft, Right);
+                    if (!IsEmerged(newLeft, Left))
+                        return new OneItemTree(Item, newLeft, Right);
                     var emergedLeft = (OneItemTree)newLeft;
-                    return new TwoItemsTree(emergedLeft.LeftItem, LeftItem, emergedLeft.Left, emergedLeft.Right, Right);
+                    return new TwoItemsTree(emergedLeft.Item, Item, emergedLeft.Left, emergedLeft.Right, Right);
                     // otherwise just reattach new left
                 }
                 //else if (item.Hash > LeftItem.Hash)
                 {
                     var newRight = Right.AddOrUpdate(item);
-                    if (!IsEmerged(newRight, Right)) 
-                        return new OneItemTree(LeftItem, Left, newRight);
+                    if (!IsEmerged(newRight, Right))
+                        return new OneItemTree(Item, Left, newRight);
                     var emergedRight = (OneItemTree)newRight;
-                    return new TwoItemsTree(LeftItem, emergedRight.LeftItem, Left, emergedRight.Left, emergedRight.Right);
+                    return new TwoItemsTree(Item, emergedRight.Item, Left, emergedRight.Left, emergedRight.Right);
                 }
+            }
+
+            public INode LookFor(int hash)
+            {
+                return hash == Item.Hash ? Item : hash < Item.Hash ? Left : Right;
             }
         }
 
-        public class TwoItemsLeaf : OneItemLeaf
+        public class TwoItemsLeaf : INode
         {
-            public override NodeType Type { get { return NodeType.TwoItemsLeaf; } }
-            public readonly Item RightItem;
+            public virtual NodeType Type { get { return NodeType.TwoItemsLeaf; } }
+            public readonly OneItemLeaf LeftItem, RightItem;
 
-            public TwoItemsLeaf(Item leftItem, Item rightItem) : base(leftItem)
+            public TwoItemsLeaf(OneItemLeaf leftItem, OneItemLeaf rightItem)
             {
+                LeftItem = leftItem;
                 RightItem = rightItem;
             }
 
-            public override INode AddOrUpdate(Item item)
+            public virtual INode AddOrUpdate(OneItemLeaf item)
             {
                 if (item.Hash < LeftItem.Hash)
-                    return new OneItemTree(LeftItem, new OneItemLeaf(item), new OneItemLeaf(RightItem));
+                    return new OneItemTree(LeftItem, item, RightItem);
                 // for now skip if (item.Hash == LeftItem.Hash)
                 if (item.Hash < RightItem.Hash)
-                    return new OneItemTree(item, new OneItemLeaf(LeftItem), new OneItemLeaf(RightItem));
+                    return new OneItemTree(item, LeftItem, RightItem);
                 // for now skip if (item.Hash == RightItem.Hash)
                 // else if (item.Hash > RightItem.Hash)
-                    return new OneItemTree(RightItem, new OneItemLeaf(LeftItem), new OneItemLeaf(item));
+                    return new OneItemTree(RightItem, LeftItem, item);
+            }
+
+            public virtual INode LookFor(int hash)
+            {
+                return hash == LeftItem.Hash ? LeftItem : RightItem;
             }
         }
 
@@ -170,14 +217,15 @@ namespace DryIoc.Playground
             public override NodeType Type { get { return NodeType.TwoItemsTree; }}
             public readonly INode Left, Middle, Right;
 
-            public TwoItemsTree(Item leftItem, Item rightItem, INode left, INode middle, INode right) : base(leftItem, rightItem)
+            public TwoItemsTree(OneItemLeaf leftItem, OneItemLeaf rightItem, INode left, INode middle, INode right)
+                : base(leftItem, rightItem)
             {
                 Left = left;
                 Middle = middle;
                 Right = right;
             }
 
-            public override INode AddOrUpdate(Item item)
+            public override INode AddOrUpdate(OneItemLeaf item)
             {
                 if (item.Hash < LeftItem.Hash)
                 {
@@ -193,7 +241,7 @@ namespace DryIoc.Playground
                     if (!IsEmerged(newMiddle, Middle))
                         return new TwoItemsTree(LeftItem, RightItem, Left, newMiddle, Right);
                     var emergedMiddle = (OneItemTree)newMiddle;
-                    return new OneItemTree(emergedMiddle.LeftItem,
+                    return new OneItemTree(emergedMiddle.Item,
                         new OneItemTree(LeftItem, Left, emergedMiddle.Right),
                         new OneItemTree(RightItem, emergedMiddle.Left, Right));
                 }
@@ -205,6 +253,15 @@ namespace DryIoc.Playground
                         ? new TwoItemsTree(LeftItem, RightItem, Left, Middle, newRight)
                         : (INode) new OneItemTree(RightItem, new OneItemTree(LeftItem, Left, Middle), newRight);
                 }
+            }
+
+            public override INode LookFor(int hash)
+            {
+                return hash == LeftItem.Hash ? LeftItem 
+                    : hash == RightItem.Hash ? RightItem
+                    : hash < LeftItem.Hash ? Left
+                    : hash > RightItem.Hash ? Right
+                    : Middle;
             }
         }
 
