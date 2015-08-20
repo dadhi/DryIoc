f7f2f425966415e1f9fa2446546f3d9daf0496d8
dadhi <none@none>
2015-08-20 09:58:56 +0300

    added: build-2015.bat
    --HG--
    branch : dev

--- BuildScripts/Build.bat
+++ BuildScripts/Build.bat
@@ -1,7 +1,12 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 set SLN="..\DryIoc.sln"
 set OUTDIR="..\bin\Release"
+
+set MSBUILDVER=%2
+if "%MSBUILDVER%"=="" set MSBUILDVER=14
+echo:MsBuild version: %MSBUILDVER%
 
 echo:
 echo:Building %SLN% into %OUTDIR% . . .
@@ -9,7 +14,7 @@ echo:Building %SLN% into %OUTDIR% . . .
 rem MSBuild 32-bit operating systems:
 rem HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0
 
-for /f "tokens=2*" %%S in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\14.0 /v MSBuildToolsPath') do (
+for /f "tokens=2*" %%S in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\%MSBUILDVER%.0 /v MSBuildToolsPath') do (
 
 	if exist "%%T" (
 
@@ -27,4 +32,5 @@ for /f "tokens=2*" %%S in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuil
     )
 )
 
+endlocal
 if not "%1"=="-nopause" pause
