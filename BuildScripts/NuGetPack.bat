f3e7276ed26882aa8ac21f3c15ca548cb501c666
dadhi <none@none>
2015-11-19 10:37:34 +0300

    changed: CI script for V2 release

--- BuildScripts/NuGetPack.bat
+++ BuildScripts/NuGetPack.bat
@@ -15,79 +15,69 @@ call :ParseVersion "DryIoc\Properties\AssemblyInfo.cs"
 echo:DryIoc v%VER%
 echo:================
 %NUGET% pack "NuGet\DryIoc.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive 
-rem -Symbols
+%NUGET% pack "NuGet\DryIoc.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "DryIocAttributes\Properties\AssemblyInfo.cs"
 echo:DryIocAttributes v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIocAttributes.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIocAttributes.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols 
+%NUGET% pack "NuGet\DryIocAttributes.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols 
 
 echo:
 call :ParseVersion "DryIoc.MefAttributedModel\Properties\AssemblyInfo.cs"
 echo:MefAttributedModel v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.MefAttributedModel.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.MefAttributedModel.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols 
+%NUGET% pack "NuGet\DryIoc.MefAttributedModel.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols 
 
 echo:
 call :ParseVersion "Extensions\DryIoc.CommonServiceLocator\Properties\AssemblyInfo.cs"
 echo:DryIoc.CommonServiceLocator v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.CommonServiceLocator.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.CommonServiceLocator.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols 
+%NUGET% pack "NuGet\DryIoc.CommonServiceLocator.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols 
 
 echo:
 call :ParseVersion "Extensions\DryIoc.Web\Properties\AssemblyInfo.cs"
 echo:DryIoc.Web v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.Web.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.Web.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols
+%NUGET% pack "NuGet\DryIoc.Web.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "Extensions\DryIoc.Mvc\Properties\AssemblyInfo.cs"
 echo:DryIoc.Mvc v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.Mvc.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.Mvc.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols
+%NUGET% pack "NuGet\DryIoc.Mvc.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "Extensions\DryIoc.WebApi\Properties\AssemblyInfo.cs"
 echo:DryIoc.WebApi v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.WebApi.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.WebApi.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols
+%NUGET% pack "NuGet\DryIoc.WebApi.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "Extensions\DryIoc.Owin\Properties\AssemblyInfo.cs"
 echo:DryIoc.Owin v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.Owin.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.Owin.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols
+%NUGET% pack "NuGet\DryIoc.Owin.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "Extensions\DryIoc.WebApi.Owin\Properties\AssemblyInfo.cs"
 echo:DryIoc.WebApi.Owin v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIoc.WebApi.Owin.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-%NUGET% pack "NuGet\DryIoc.WebApi.Owin.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols
+%NUGET% pack "NuGet\DryIoc.WebApi.Owin.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "Net45\DryIocZero\Properties\AssemblyInfo.cs"
 echo:DryIocZero v%VER%
 echo:============================
 %NUGET% pack "NuGet\DryIocZero.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
-rem -Symbols
 
 echo: 
 echo:Packaging succeeded.
