f634c95bc7306c7dd6d9488ddd83f422c8e692d7
dadhi <none@none>
2015-12-01 13:16:41 +0300

    changed: Version to 2.0.2

--- DryIoc/Properties/AssemblyInfo.cs
+++ DryIoc/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
-[assembly: AssemblyInformationalVersion("2.0.1")]
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: AssemblyInformationalVersion("2.0.2")]
