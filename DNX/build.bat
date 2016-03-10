4c93d617d41900de0e65c4c01a3f28b88904c177
dadhi <max.volkov@gmail.com>
2016-03-10 14:23:20 +0300

    changed: Updated Dnx DI and Mef versions to 2.2.2
    --HG--
    branch : dev

--- DNX/build.bat
+++ DNX/build.bat
@@ -4,14 +4,14 @@ echo:BUILD AND RUN TESTS:
 rem Restores packages for the first time
 rem call dnu restore
 
-dnx -p "DryIoc.Dnx.UnitTests" test
-dnx -p "DryIoc.MefAttributedModel.Dnx.UnitTests" test
+rem dnx -p "DryIoc.Dnx.UnitTests" test
+rem dnx -p "DryIoc.MefAttributedModel.Dnx.UnitTests" test
 rem dnx -p "DryIoc.Dnx.DependencyInjection.Specification.Tests" test
 
 echo:BUILD AND PACKAGE:
 call dnu pack DryIoc.Dnx --configuration Release
-call dnu pack DryIoc.Dnx.DependencyInjection --configuration Release
 call dnu pack DryIocAttributes.Dnx --configuration Release
 call dnu pack DryIoc.MefAttributedModel.Dnx --configuration Release
+call dnu pack DryIoc.Dnx.DependencyInjection --configuration Release
 
 pause
