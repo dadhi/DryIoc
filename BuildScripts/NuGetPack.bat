85afb97520dbc603e8c7fac1bf0548e146fc6c9a
dadhi <none@none>
2016-06-13 12:40:06 +0300

    changed: DryIoc.AspNetCore.DependencyInjection 1.0.0-rc2 with all spec test passing. added: build script for AspNetCore.DI added: NuGetPublish back to build scripts.
    --HG--
    branch : dev

--- BuildScripts/NuGetPack.bat
+++ BuildScripts/NuGetPack.bat
@@ -16,7 +16,7 @@ echo:DryIoc v%VER%
 echo:================
 %NUGET% pack "NuGet\DryIoc.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive
 %NUGET% pack "NuGet\DryIoc.dll.nuspec" -Version %VER% -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
-%NUGET% pack "NuGet\DryIoc.NetCore.dll.nuspec" -Version %VER%-netcore-rc2 -OutputDirectory %PACKAGEDIR% -NonInteractive
+%NUGET% pack "NuGet\DryIoc.NetCore.dll.nuspec" -Version %VER%-netcore-rc2 -OutputDirectory %PACKAGEDIR% -NonInteractive -Symbols
 
 echo:
 call :ParseVersion "DryIocAttributes\Properties\AssemblyInfo.cs"
