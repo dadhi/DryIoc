2e8bb5bf77cc347b486d36cc92956be2689aef85
yallie <yallie@yandex.ru>
2016-06-15 21:47:21 +0300

    Added ImportMany unit tests with reference Mef implementation, #291.
    --HG--
    branch : dev

---
+++ Net40/DryIoc.MefAttributedModel.UnitTests/ImportManyTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DryIoc.MefAttributedModel.UnitTests.CUT;
+using NUnit.Framework;
+
+namespace DryIoc.MefAttributedModel.UnitTests
+{
+    [TestFixture]
+    public class ImportManyTests
+    {
+        private CompositionContainer Mef { get; } = new CompositionContainer(new AssemblyCatalog(typeof(IPasswordHasher).Assembly));
+
+        private IContainer Container => CreateContainer();
+
+        private IContainer CreateContainer()
+        {
+            var container = new Container().WithMefAttributedModel();
+            container.RegisterExports(new[] { typeof(IPasswordHasher).Assembly });
+            return container;
+        }
+
+        [Test]
+        private void ImportMany_works_with_importing_constructor_in_Mef1()
+        {
+            var pw = Mef.GetExport<PasswordVerifier1>();
+
+            Assert.NotNull(pw);
+            Assert.NotNull(pw.Value);
+            Assert.AreEqual(3, pw.Value.Hashers.Count());
+            Assert.IsTrue(pw.Value.ImportsSatisfied);
+        }
+
+        [Test]
+        private void ImportMany_works_with_importing_constructor_in_Mef2()
+        {
+            var pw = Mef.GetExport<PasswordVerifier2>();
+
+            Assert.NotNull(pw);
+            Assert.NotNull(pw.Value);
+            Assert.AreEqual(3, pw.Value.Hashers.Count());
+            Assert.IsTrue(pw.Value.ImportsSatisfied);
+        }
+
+        [Test]
+        private void ImportMany_works_with_property_injection_in_Mef3()
+        {
+            var pw = Mef.GetExport<PasswordVerifier3>();
+
+            Assert.NotNull(pw);
+            Assert.NotNull(pw.Value);
+            Assert.AreEqual(3, pw.Value.Hashers.Count());
+            Assert.IsTrue(pw.Value.ImportsSatisfied);
+        }
+
+        [Test]
+        private void ImportMany_works_with_property_injection_in_Mef4()
+        {
+            var pw = Mef.GetExport<PasswordVerifier4>();
+
+            Assert.NotNull(pw);
+            Assert.NotNull(pw.Value);
+            Assert.AreEqual(3, pw.Value.Hashers.Count());
+            Assert.IsTrue(pw.Value.ImportsSatisfied);
+        }
+    }
+}
