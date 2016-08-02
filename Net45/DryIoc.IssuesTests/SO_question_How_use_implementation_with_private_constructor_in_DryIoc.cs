2c3e6b90c790c010759a349205a4aae93f87411f
dadhi <none@none>
2016-08-02 17:41:38 +0300

    added: POC for the #313 Support non public constructors with ConstructorWithResolvableArguments and for SO question http://stackoverflow.com/questions/38687161/how-use-implementation-with-private-constructor-in-dryioc/38687867
    --HG--
    branch : dev

---
+++ Net45/DryIoc.IssuesTests/SO_question_How_use_implementation_with_private_constructor_in_DryIoc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class SO_question_How_use_implementation_with_private_constructor_in_DryIoc
+    {
+        [Test]
+        public void Test()
+        {
+            var c = new Container();
+            c.RegisterMany(new[] {typeof(A), typeof(B)},
+                made: Made.Of(SelectMostResolvablePrivateConstructor(includeNonPublic: true)));
+
+            c.Resolve<B>();
+        }
+
+        private FactoryMethodSelector SelectMostResolvablePrivateConstructor(bool includeNonPublic = false)
+        {
+            return request =>
+            {
+                var implementationType = request.ImplementationType.ThrowIfNull();
+                var ctors = implementationType.GetAllConstructors(includeNonPublic: true).ToArrayOrSelf();
+                if (ctors.Length == 0)
+                    return null; // Delegate handling of constructor absence to the Caller code.
+                if (ctors.Length == 1)
+                    return FactoryMethod.Of(ctors[0]);
+
+                var ctorsWithMoreParamsFirst = ctors
+                    .Select(c => new {Ctor = c, Params = c.GetParameters()})
+                    .OrderByDescending(x => x.Params.Length);
+
+                var rules = request.Container.Rules;
+                var parameterSelector = rules.Parameters.And(request.Made.Parameters)(request);
+
+                var matchedCtor = ctorsWithMoreParamsFirst.FirstOrDefault(x =>
+                        x.Params.All(p => IsResolvableParameter(p, parameterSelector, request)));
+
+                var ctor = matchedCtor.ThrowIfNull(Error.UnableToFindCtorWithAllResolvableArgs, request).Ctor;
+
+                return FactoryMethod.Of(ctor);
+            };
+        }
+
+        private static bool IsResolvableParameter(ParameterInfo parameter,
+            Func<ParameterInfo, ParameterServiceInfo> parameterSelector, Request request)
+        {
+            var parameterServiceInfo = parameterSelector(parameter) ?? ParameterServiceInfo.Of(parameter);
+            var parameterRequest = request.Push(parameterServiceInfo.WithDetails(ServiceDetails.IfUnresolvedReturnDefault, request));
+
+            if (parameterServiceInfo.Details.HasCustomValue)
+            {
+                var customValue = parameterServiceInfo.Details.CustomValue;
+                return customValue == null
+                    || customValue.GetType().IsAssignableTo(parameterRequest.ServiceType);
+            }
+
+            var parameterFactory = request.Container.ResolveFactory(parameterRequest);
+            return parameterFactory != null && parameterFactory.GetExpressionOrDefault(parameterRequest) != null;
+        }
+
+        public class A { internal A() {} }
+
+        public class B { internal B(A a) {} }
+    }
+}
