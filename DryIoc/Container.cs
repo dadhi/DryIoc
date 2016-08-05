016fc9d6a5e9aee5c528abd097211e8615db3801
dadhi <max.volkov@gmail.com>
2016-08-05 10:58:07 +0300

    fixed: #313: Support non public constructors with ConstructorWithResolvableArguments
    --HG--
    branch : dev

--- DryIoc/Container.cs
+++ DryIoc/Container.cs
@@ -3104,64 +3104,79 @@ namespace DryIoc
                 .Append("::").Append(ConstructorOrMethodOrMember).ToString();
         }
 
-        /// <summary>Searches for constructor with all resolvable parameters or throws <see cref="ContainerException"/> if not found.
-        /// Works both for resolving as service and as Func of TArgs... returning TService.</summary>
-        public static readonly FactoryMethodSelector ConstructorWithResolvableArguments = request =>
+        private static FactoryMethodSelector Constructor(bool includeNonPublic = false)
         {
-            var implementationType = request.ImplementationType.ThrowIfNull();
-            var ctors = implementationType.GetPublicInstanceConstructors().ToArrayOrSelf();
-            if (ctors.Length == 0)
-                return null; // Delegate handling of constructor absence to the Caller code.
-            if (ctors.Length == 1)
-                return Of(ctors[0]);
-
-            var ctorsWithMoreParamsFirst = ctors
-                .Select(c => new { Ctor = c, Params = c.GetParameters() })
-                .OrderByDescending(x => x.Params.Length);
-
-            var rules = request.Container.Rules;
-            var parameterSelector = rules.Parameters.And(request.Made.Parameters)(request);
-
-            if (!request.IsWrappedInFuncWithArgs(immediateParent: true))
+            return request =>
             {
-                var matchedCtor = ctorsWithMoreParamsFirst.FirstOrDefault(x =>
-                    x.Params.All(p => IsResolvableParameter(p, parameterSelector, request)));
-                var ctor = matchedCtor.ThrowIfNull(Error.UnableToFindCtorWithAllResolvableArgs, request).Ctor;
-                return Of(ctor);
-            }
-            else
-            {
-                // For Func with arguments, 
-                // match constructor should contain all input arguments and 
-                // the rest should be resolvable.
-                var funcType = request.ParentOrWrapper.ServiceType;
-                var funcArgs = funcType.GetGenericParamsAndArgs();
-                var inputArgCount = funcArgs.Length - 1;
+                var implementationType = request.ImplementationType.ThrowIfNull();
+                var ctors = implementationType.GetAllConstructors(includeNonPublic).ToArrayOrSelf();
+                if (ctors.Length == 0)
+                    return null; // Delegate handling of constructor absence to the Caller code.
+                if (ctors.Length == 1)
+                    return Of(ctors[0]);
 
-                var matchedCtor = ctorsWithMoreParamsFirst
-                    .Where(x => x.Params.Length >= inputArgCount)
-                    .FirstOrDefault(x =>
-                    {
-                        var matchedIndecesMask = 0;
-                        return x.Params.Except(
-                            x.Params.Where(p =>
-                            {
-                                var inputArgIndex = funcArgs.IndexOf(p.ParameterType);
-                                if (inputArgIndex == -1 || inputArgIndex == inputArgCount ||
-                                    (matchedIndecesMask & inputArgIndex << 1) != 0)
+                var ctorsWithMoreParamsFirst = ctors
+                    .Select(c => new { Ctor = c, Params = c.GetParameters() })
+                    .OrderByDescending(x => x.Params.Length);
+
+                var rules = request.Container.Rules;
+                var parameterSelector = rules.Parameters.And(request.Made.Parameters)(request);
+
+                if (!request.IsWrappedInFuncWithArgs(immediateParent: true))
+                {
+                    var matchedCtor = ctorsWithMoreParamsFirst.FirstOrDefault(x =>
+                        x.Params.All(p => IsResolvableParameter(p, parameterSelector, request)));
+                    var ctor = matchedCtor.ThrowIfNull(Error.UnableToFindCtorWithAllResolvableArgs, request).Ctor;
+                    return Of(ctor);
+                }
+                else
+                {
+                    // For Func with arguments, 
+                    // match constructor should contain all input arguments and 
+                    // the rest should be resolvable.
+                    var funcType = request.ParentOrWrapper.ServiceType;
+                    var funcArgs = funcType.GetGenericParamsAndArgs();
+                    var inputArgCount = funcArgs.Length - 1;
+
+                    var matchedCtor = ctorsWithMoreParamsFirst
+                        .Where(x => x.Params.Length >= inputArgCount)
+                        .FirstOrDefault(x =>
+                        {
+                            var matchedIndecesMask = 0;
+                            return x.Params.Except(
+                                x.Params.Where(p =>
+                                {
+                                    var inputArgIndex = funcArgs.IndexOf(p.ParameterType);
+                                    if (inputArgIndex == -1 || inputArgIndex == inputArgCount ||
+                                        (matchedIndecesMask & inputArgIndex << 1) != 0)
                                     // input argument was already matched by another parameter
                                     return false;
-                                matchedIndecesMask |= inputArgIndex << 1;
-                                return true;
-                            })).All(p => IsResolvableParameter(p, parameterSelector, request));
-                    });
+                                    matchedIndecesMask |= inputArgIndex << 1;
+                                    return true;
+                                })).All(p => IsResolvableParameter(p, parameterSelector, request));
+                        });
 
-                var ctor = matchedCtor.ThrowIfNull(Error.UnableToFindMatchingCtorForFuncWithArgs, funcType, request).Ctor;
-                return Of(ctor);
-            }
-        };
+                    var ctor = matchedCtor.ThrowIfNull(Error.UnableToFindMatchingCtorForFuncWithArgs, funcType, request).Ctor;
+                    return Of(ctor);
+                }
 
-        private static bool IsResolvableParameter(ParameterInfo parameter,
+            };
+        }
+
+        /// <summary>Searches for public constructor with most resolvable parameters or throws <see cref="ContainerException"/> if not found.
+        /// Works both for resolving service and Func&lt;TArgs..., TService&gt;</summary>
+        public static readonly FactoryMethodSelector ConstructorWithResolvableArguments = 
+            Constructor();
+
+        /// <summary>Searches for constructor (including non public ones) with most resolvable parameters or throws <see cref="ContainerException"/> if not found.
+        /// Works both for resolving service and Func&lt;TArgs..., TService&gt;</summary>
+        public static readonly FactoryMethodSelector ConstructorWithResolvableArgumentsIncludingNonPublic = 
+            Constructor(includeNonPublic: true);
+
+        /// <summary>Checks that parameter is selected on requested path and with provided parameter selector.</summary>
+        /// <param name="parameter"></param> <param name="parameterSelector"></param> <param name="request"></param>
+        /// <returns>True if parameter is resolvable.</returns>
+        public static bool IsResolvableParameter(ParameterInfo parameter,
             Func<ParameterInfo, ParameterServiceInfo> parameterSelector, Request request)
         {
             var parameterServiceInfo = parameterSelector(parameter) ?? ParameterServiceInfo.Of(parameter);
