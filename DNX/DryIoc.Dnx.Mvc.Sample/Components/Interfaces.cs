4f2623031dd79eae99e0f69287ff98ee3d04325a
dadhi <max.volkov@gmail.com>
2016-08-31 15:21:14 +0300

    removed: DNX (.Net Core RC1) projects
    --HG--
    branch : dev

--- DNX/DryIoc.Dnx.Mvc.Sample/Components/Interfaces.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Web.Components
-{
-    public interface IServiceBase { int InstanceId { get; } }
-    public interface ISingletonService : IServiceBase { }
-    public interface ITransientService : IServiceBase { }
-    public interface IPerRequestService : IServiceBase { }
-}
