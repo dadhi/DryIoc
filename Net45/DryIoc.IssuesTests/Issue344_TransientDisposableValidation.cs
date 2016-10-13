0b701fb556b103e819a70acbe201788c43f2b257
yallie <yallie@yandex.ru>
2016-10-13 22:24:29 +0300

    Added a failing test related to transient disposable registration, #344.
    --HG--
    branch : dev

--- Net45/DryIoc.IssuesTests/Issue344_TransientDisposableValidation.cs
+++ Net45/DryIoc.IssuesTests/Issue344_TransientDisposableValidation.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Composition;
+using DryIoc.MefAttributedModel;
 using NUnit.Framework;
 
 namespace DryIoc.IssuesTests
@@ -12,13 +14,29 @@ namespace DryIoc.IssuesTests
             var container = new Container().With(r =>
                 r.WithDefaultReuseInsteadOfTransient(Reuse.Singleton));
 
-            Assert.DoesNotThrow(() => 
-            container.Register<IService, Service>());
+            Assert.DoesNotThrow(() =>
+                container.Register<IService, Service>());
+        }
+
+        [Test, Ignore("fails")]
+        public void Container_with_Mef_support_with_tracking_disposable_transients_shouldnt_complain_about_transient_disposable_registration()
+        {
+            var container = new Container().WithMef()
+                .With(rules => rules.WithTrackingDisposableTransients());
+
+            Assert.DoesNotThrow(() =>
+                container.RegisterExports(typeof(DataContext)));
         }
 
         interface IService : IDisposable { }
 
         class Service : IService
+        {
+            public void Dispose() { }
+        }
+
+        [Export, PartCreationPolicy(CreationPolicy.NonShared)]
+        class DataContext : IDisposable
         {
             public void Dispose() { }
         }
