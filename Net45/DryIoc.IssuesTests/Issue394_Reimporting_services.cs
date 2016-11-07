130371b920710ebb041e3d6677d23d1c4fc117dd
yallie <yallie@yandex.ru>
2016-11-07 14:39:08 +0300

    Added failing test cases for the reimporting issue, #394.
    --HG--
    branch : dev

---
+++ Net45/DryIoc.IssuesTests/Issue394_Reimporting_services.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.Composition;
+using DryIoc.MefAttributedModel;
+using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class Issue394_Reimporting_services
+    {
+        [Test, Ignore("fails")]
+        public void InjectPropertiesAndFields_imports_new_services_into_resolved_singleton()
+        {
+            // registered on application startup
+            var container = new Container().WithMef();
+            container.Register<Aggregator>(Reuse.Singleton);
+            container.Register<IAggregatee, Agg1>();
+
+            // used later
+            var aggregator = container.Resolve<Aggregator>();
+            Assert.AreEqual(1, aggregator.Aggregatees.Length);
+
+            // registered new service and re-imported
+            container.Register<IAggregatee, Agg2>();
+            container.InjectPropertiesAndFields(aggregator);
+            Assert.AreEqual(2, aggregator.Aggregatees.Length);
+
+            // registered new service and re-imported
+            container.Register<IAggregatee, Agg3>();
+            container.InjectPropertiesAndFields(aggregator);
+            Assert.AreEqual(3, aggregator.Aggregatees.Length);
+        }
+
+        [Test, Ignore("fails")]
+        public void InjectPropertiesAndFields_imports_new_services_into_an_instance()
+        {
+            // registered on application startup
+            var container = new Container().WithMef();
+            container.Register<IAggregatee, Agg1>();
+
+            // used later
+            var aggregator = new Aggregator();
+            container.InjectPropertiesAndFields(aggregator);
+            Assert.AreEqual(1, aggregator.Aggregatees.Length);
+
+            // registered new service and re-imported
+            container.Register<IAggregatee, Agg2>();
+            container.InjectPropertiesAndFields(aggregator);
+            Assert.AreEqual(2, aggregator.Aggregatees.Length);
+
+            // registered new service and re-imported
+            container.Register<IAggregatee, Agg3>();
+            container.InjectPropertiesAndFields(aggregator);
+            Assert.AreEqual(3, aggregator.Aggregatees.Length);
+        }
+
+        public class Aggregator
+        {
+            [ImportMany]
+            public IAggregatee[] Aggregatees { get; set; }
+        }
+
+        public interface IAggregatee { }
+
+        public class Agg1 : IAggregatee { }
+
+        public class Agg2 : IAggregatee { }
+
+        public class Agg3 : IAggregatee { }
+    }
+}
