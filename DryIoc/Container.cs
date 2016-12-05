c0a447d29b45e5ac78c723888844a349b55df3b8
maksim volkau <max.volkov@gmail.com>
2016-12-05 09:12:10 +0000

    fixed: license text

--- DryIoc/Container.cs
+++ DryIoc/Container.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 The MIT License (MIT)
 
 Copyright (c) 2013-2016 Maksim Volkau
@@ -10,7 +10,7 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included AddOrUpdateServiceFactory
+The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
