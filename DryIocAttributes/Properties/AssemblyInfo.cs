daad699c54d4e2f8cf847cd3145fcb2d42d73cfd
dadhi <none@none>
2017-03-10 12:21:13 +0300

    publishing: DryIoc v2.11.0-preview-01 and DryIoc.MefAttributedModel v3.0.0-preview-05 added: NuGet and BuildScripts folders to DryIoc.start.sln
    --HG--
    branch : dev

--- DryIocAttributes/Properties/AssemblyInfo.cs
+++ DryIocAttributes/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("DryIocAttributes")]
 [assembly: AssemblyDescription("Superset of MEF attributes. In addition to Export/Import contains attributes to support modern IOC (DryIoc) features.")]
-[assembly: AssemblyProduct("DryIocAttributes")]
 [assembly: AssemblyCopyright("Copyright © 2016 Maksim Volkau")]
 
 [assembly: AssemblyVersion(             "3.0.0")]
