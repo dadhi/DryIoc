2931909d80726029eae63ed2a70bab6fc592c362
dadhi <none@none>
2017-03-15 16:18:04 +0300

    changed: Updated DryIoc netstandard package dependencies to v4.3
    --HG--
    branch : dev

--- DryIoc/Properties/AssemblyInfo.cs
+++ DryIoc/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@
 
 [assembly: AssemblyVersion(             "2.11.0")]
 [assembly: AssemblyFileVersion(         "2.11.0")]
-[assembly: AssemblyInformationalVersion("2.11.0-preview-01")]
+[assembly: AssemblyInformationalVersion("2.11.0-preview-02")]
