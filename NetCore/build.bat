ed7fe86f78254fc311243a40058dc6bb5bb3840a
dadhi <none@none>
2017-04-10 12:03:38 +0300

    changed: DryIoc netstandard to be built only for netstandard1.0 fixed: DryIoc.NetCore.UnitTests

--- NetCore/build.bat
+++ NetCore/build.bat
@@ -4,6 +4,7 @@ dotnet restore
 
 dotnet build
 
+dotnet test ".\test\DryIoc.NetCore.UnitTests"
 dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests"
 
 dotnet pack ".\src\DryIoc.Microsoft.DependencyInjection" -c Release -o "..\bin\NuGetPackages"
