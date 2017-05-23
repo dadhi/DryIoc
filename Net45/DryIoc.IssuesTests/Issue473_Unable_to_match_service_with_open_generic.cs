9e47eab4d0cd2e4950e59b2aee3be7fab7838816
dadhi <none@none>
2017-05-23 16:13:55 +0300

    added: Failing test for #473
    --HG--
    branch : dev

---
+++ Net45/DryIoc.IssuesTests/Issue473_Unable_to_match_service_with_open_generic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class Issue473_Unable_to_match_service_with_open_generic
+    {
+        [Test, Ignore("open issue, not sure does it need to be fixed")]
+        public void Unable_to_match_in_ResolveMany_of_array()
+        {
+            var container = new Container();
+            container.RegisterMany(new[] { typeof(MyDictionary<>) });
+
+            var array = container.ResolveMany(typeof(IMyInterface), ResolveManyBehavior.AsFixedArray);
+            Assert.IsEmpty(array);
+        }
+
+        public class MyDictionary<T> : IEnumerable<KeyValuePair<string, T>>
+        {
+            public IEnumerator<KeyValuePair<string, T>> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface IMyInterface {}
+    }
+}
