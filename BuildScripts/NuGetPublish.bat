fe83eafa9d0c64236d3899d949a3d5ceeeb712f7
dadhi <max.volkov@gmail.com>
2017-07-15 22:32:06 +0300

    released: DryIocZero.3.0.0-preview-03
    --HG--
    branch : dev

--- BuildScripts/NuGetPublish.bat
+++ BuildScripts/NuGetPublish.bat
@@ -6,23 +6,23 @@ set PACKAGEDIR="bin\NuGetPackages"
 set /p APIKEY=<ApiKey.txt
 
 rem DryIoc
-%NUGET% push "%PACKAGEDIR%\DryIoc.dll.2.11.5.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
-%NUGET% push "%PACKAGEDIR%\DryIoc.2.11.5.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
-%NUGET% push "%PACKAGEDIR%\DryIoc.Internal.2.11.5.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
+rem %NUGET% push "%PACKAGEDIR%\DryIoc.dll.2.11.5.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
+rem %NUGET% push "%PACKAGEDIR%\DryIoc.2.11.5.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
+rem %NUGET% push "%PACKAGEDIR%\DryIoc.Internal.2.11.5.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
 
 rem DryIoc.Microsoft.DependencyInjection
 rem %NUGET% push "%PACKAGEDIR%\DryIoc.Microsoft.DependencyInjection.1.0.2.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
 
 rem DryIocZero
-rem %NUGET% push "%PACKAGEDIR%\DryIocZero.3.0.0-preview-01.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
+%NUGET% push "%PACKAGEDIR%\DryIocZero.3.0.0-preview-03.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
 
 rem DryIocAttributes
 rem %NUGET% push "%PACKAGEDIR%\DryIocAttributes.3.1.0.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
 rem %NUGET% push "%PACKAGEDIR%\DryIocAttributes.dll.3.1.0.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
 
 rem DryIoc.MefAttributedModel
-%NUGET% push "%PACKAGEDIR%\DryIoc.MefAttributedModel.3.1.2.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
-%NUGET% push "%PACKAGEDIR%\DryIoc.MefAttributedModel.dll.3.1.2.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
+rem %NUGET% push "%PACKAGEDIR%\DryIoc.MefAttributedModel.3.1.2.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
+rem %NUGET% push "%PACKAGEDIR%\DryIoc.MefAttributedModel.dll.3.1.2.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
 
 rem DryIoc.Web
 rem %NUGET% push "%PACKAGEDIR%\DryIoc.Web.2.2.0.nupkg" -Source https://nuget.org -ApiKey %APIKEY%
