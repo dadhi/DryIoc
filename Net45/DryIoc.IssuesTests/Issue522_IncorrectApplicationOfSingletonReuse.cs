d59c2f0e20263f3f750f69e0b89962ad00487188
yallie <yallie@yandex.ru>
2017-09-16 21:57:34 +0300

    Added working samples with the alternative approach, #522.

--- Net45/DryIoc.IssuesTests/Issue522_IncorrectApplicationOfSingletonReuse.cs
+++ Net45/DryIoc.IssuesTests/Issue522_IncorrectApplicationOfSingletonReuse.cs
@@ -9,9 +9,9 @@ namespace DryIoc.IssuesTests
     class Issue522_IncorrectApplicationOfSingletonReuse
     {
         [Test]
-        public void MefCreatesDifferentInstancesOfNamedDriver()
+        public void MefCreatesDifferentInstancesOfNamedDriverUsingStaticFactory()
         {
-            var catalog = new TypeCatalog(typeof(Driver.DriverFactory));
+            var catalog = new TypeCatalog(typeof(Driver.StaticDriverFactory));
             var container = new CompositionContainer(catalog);
 
             var factory = container.GetExport<Func<string, IDriver>>().Value;
@@ -23,11 +23,26 @@ namespace DryIoc.IssuesTests
             Assert.AreNotSame(driver1, driver2);
         }
 
+        [Test]
+        public void MefCreatesDifferentInstancesOfNamedDriverUsingNonStaticFactory()
+        {
+            var catalog = new TypeCatalog(typeof(Driver.DriverFactory));
+            var container = new CompositionContainer(catalog);
+
+            var factory = container.GetExport<IDriverFactory>().Value;
+            var driver1 = factory.Create("One");
+            Assert.AreEqual("One", driver1.Name);
+
+            var driver2 = factory.Create("Two");
+            Assert.AreEqual("Two", driver2.Name);
+            Assert.AreNotSame(driver1, driver2);
+        }
+
         [Test, Ignore("TODO: fix")]
-        public void DryIocWithDefaultSettingsCreatesDifferentInstancesOfNamedDriver()
+        public void DryIocWithDefaultSettingsCreatesDifferentInstancesOfNamedDriverUsingStaticFactory()
         {
             var container = new Container().WithMef();
-            container.RegisterExports(typeof(Driver.DriverFactory));
+            container.RegisterExports(typeof(Driver.StaticDriverFactory));
 
             var factory = container.Resolve<Func<string, IDriver>>();
             var driver1 = factory("One");
@@ -39,25 +54,25 @@ namespace DryIoc.IssuesTests
         }
 
         [Test]
-        public void DryIocWithTransientDefaultReuseCreatesDifferentInstancesOfNamedDriver()
+        public void DryIocWithDefaultSettingsCreatesDifferentInstancesOfNamedDriverUsingNonStaticFactory()
         {
-            var container = new Container().WithMef().With(rules => rules.WithDefaultReuse(Reuse.Transient));
+            var container = new Container().WithMef();
             container.RegisterExports(typeof(Driver.DriverFactory));
 
-            var factory1 = container.Resolve<Func<string, IDriver>>();
-            var driver1 = factory1("One");
+            var factory = container.Resolve<IDriverFactory>();
+            var driver1 = factory.Create("One");
             Assert.AreEqual("One", driver1.Name);
 
-            var driver2 = factory1("Two");
+            var driver2 = factory.Create("Two");
             Assert.AreEqual("Two", driver2.Name);
             Assert.AreNotSame(driver1, driver2);
         }
 
-        [Test, Ignore("TODO: fix")]
-        public void DryIocWithScopedDefaultReuseCreatesDifferentInstancesOfNamedDriver()
+        [Test]
+        public void DryIocWithTransientDefaultReuseCreatesDifferentInstancesOfNamedDriverUsingStaticFactory()
         {
-            var container = new Container().WithMef().With(rules => rules.WithImplicitRootOpenScope().WithDefaultReuseInsteadOfTransient(Reuse.InCurrentScope));
-            container.RegisterExports(typeof(Driver.DriverFactory));
+            var container = new Container().WithMef().With(rules => rules.WithDefaultReuse(Reuse.Transient));
+            container.RegisterExports(typeof(Driver.StaticDriverFactory));
 
             var factory = container.Resolve<Func<string, IDriver>>();
             var driver1 = factory("One");
@@ -68,9 +83,59 @@ namespace DryIoc.IssuesTests
             Assert.AreNotSame(driver1, driver2);
         }
 
+        [Test]
+        public void DryIocWithTransientDefaultReuseCreatesDifferentInstancesOfNamedDriverUsingNonStaticFactory()
+        {
+            var container = new Container().WithMef().With(rules => rules.WithDefaultReuse(Reuse.Transient));
+            container.RegisterExports(typeof(Driver.DriverFactory));
+
+            var factory = container.Resolve<IDriverFactory>();
+            var driver1 = factory.Create("One");
+            Assert.AreEqual("One", driver1.Name);
+
+            var driver2 = factory.Create("Two");
+            Assert.AreEqual("Two", driver2.Name);
+            Assert.AreNotSame(driver1, driver2);
+        }
+
+        [Test, Ignore("TODO: fix")]
+        public void DryIocWithScopedDefaultReuseCreatesDifferentInstancesOfNamedDriverUsingStaticFactory()
+        {
+            var container = new Container().WithMef().With(rules => rules.WithImplicitRootOpenScope().WithDefaultReuseInsteadOfTransient(Reuse.InCurrentScope));
+            container.RegisterExports(typeof(Driver.StaticDriverFactory));
+
+            var factory = container.Resolve<Func<string, IDriver>>();
+            var driver1 = factory("One");
+            Assert.AreEqual("One", driver1.Name);
+
+            var driver2 = factory("Two");
+            Assert.AreEqual("Two", driver2.Name);
+            Assert.AreNotSame(driver1, driver2);
+        }
+
+        [Test]
+        public void DryIocWithScopedDefaultReuseCreatesDifferentInstancesOfNamedDriverUsingNonStaticFactory()
+        {
+            var container = new Container().WithMef().With(rules => rules.WithImplicitRootOpenScope().WithDefaultReuseInsteadOfTransient(Reuse.InCurrentScope));
+            container.RegisterExports(typeof(Driver.DriverFactory));
+
+            var factory = container.Resolve<IDriverFactory>();
+            var driver1 = factory.Create("One");
+            Assert.AreEqual("One", driver1.Name);
+
+            var driver2 = factory.Create("Two");
+            Assert.AreEqual("Two", driver2.Name);
+            Assert.AreNotSame(driver1, driver2);
+        }
+
         interface IDriver
         {
             string Name { get; }
+        }
+
+        interface IDriverFactory
+        {
+            IDriver Create(string name);
         }
 
         class Driver : IDriver
@@ -82,10 +147,19 @@ namespace DryIoc.IssuesTests
 
             public string Name { get; }
 
-            public static class DriverFactory
+            public static class StaticDriverFactory
             {
                 [Export]
                 public static IDriver Create(string Name)
+                {
+                    return new Driver(Name);
+                }
+            }
+
+            [Export(typeof(IDriverFactory))]
+            public class DriverFactory : IDriverFactory
+            {
+                public IDriver Create(string Name)
                 {
                     return new Driver(Name);
                 }
