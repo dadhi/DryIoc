e2ffb7bd6ab7068f611071265b6621b0da46ea94
Maksim Volkau <max.volkov@gmail.com>
2017-12-29 14:05:24 +0300

    added: Test for #548
    --HG--
    branch : dev

---
+++ Net45/DryIoc.IssuesTests/Issue548_After_registering_a_factory_Func_is_returned_instead_of_the_result_of_Func.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+#if FEC_EXPRESSION_INFO
+    using static FastExpressionCompiler.ExpressionInfo;
+#else
+    using static System.Linq.Expressions.Expression;
+#endif
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class Issue548_After_registering_a_factory_Func_is_returned_instead_of_the_result_of_Func
+    {
+        [Test]
+        public void Test()
+        {
+            var c = new Container(rules => rules.
+                WithUnknownServiceResolvers(request =>
+                {
+                    var optsType = typeof(IOptions<>).MakeGenericType(request.ServiceType);
+                    if (!request.Container.IsRegistered(optsType))
+                        return null;
+
+                    var opts = request.Container.Resolve(optsType);
+                    return new ExpressionFactory(_ => Property(Constant(opts), optsType.Property("Value")));
+                }));
+
+            c.UseInstance<IOptions<Foo>>(new Options<Foo> { Value = new Foo() });
+            c.Register<FooUser>();
+
+            var fooUser = c.Resolve<FooUser>();
+            Assert.IsNotNull(fooUser.Foo);
+        }
+
+        public interface IOptions<T> { T Value { get; } }
+
+        class Options<T> : IOptions<T>
+        {
+            public T Value { get; set; }
+        }
+
+        public class Foo { }
+
+        public class FooUser
+        {
+            public Foo Foo { get; }
+            public FooUser(Foo foo)
+            {
+                Foo = foo;
+            }
+        }
+    }
+}
