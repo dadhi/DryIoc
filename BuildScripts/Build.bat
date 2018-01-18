428f845cb663245557efdaf924645fedc677e278
Maksim Volkau <max.volkov@gmail.com>
2018-01-18 10:11:45 +0300

    added: wip: NetStandard 1.3 target to add FEC and AsyncExecutionFlowScopeContext changed: MSBuild15 as default build tool in script.
    --HG--
    branch : dev

--- BuildScripts/Build.bat
+++ BuildScripts/Build.bat
@@ -5,9 +5,10 @@ set SLN="..\DryIoc.sln"
 set OUTDIR="..\bin\Release"
 set NUGET="..\.nuget\NuGet.exe"
 
+set NOPAUSE=%1
 set MSBUILDVER=%2
 if "%MSBUILDVER%"=="" set MSBUILDVER=14
-echo:MsBuild version: %MSBUILDVER%
+echo:Default MSBuild version is 15, fallback version is: %MSBUILDVER%
 
 echo:
 echo:Restoring packages for solution %SLN% . . .
@@ -16,12 +17,28 @@ echo:Restoring packages for solution %SLN% . . .
 echo:
 echo:Building %SLN% into %OUTDIR% . . .
 
+set MSBUILD15="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\bin\MSBuild.exe" 
+echo:First try to find MSBuild 15: %MSBUILD15%
+if exist %MSBUILD15% (
+	echo:OK, MsBuild15 is present. Start building . . .
+
+	%MSBUILD15% %SLN% /t:Rebuild /v:minimal /m /fl /flp:LogFile=MSBuild.log ^
+		/p:OutDir=%OUTDIR% ^
+		/p:GenerateProjectSpecificOutputFolder=false ^
+		/p:Configuration=Release ^
+		/p:RestorePackages=false 
+
+	find /C "Build succeeded." MSBuild.log
+	goto :eoscript
+) 
+
+echo:MsBuild15 is not found. Try other versions found in Win Registry.
 for /f "tokens=2*" %%S in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\%MSBUILDVER%.0 /v MSBuildToolsPath') do (
 
 	if exist "%%T" (
 
 		echo:
-		echo:Using MSBuild from "%%T"
+		echo:Using MSBuild from path "%%T\MSBuild.exe"
 
 		"%%T\MSBuild.exe" %SLN% /t:Rebuild /v:minimal /m /fl /flp:LogFile=MSBuild.log ^
    			/p:OutDir=%OUTDIR% ^
@@ -34,4 +51,5 @@ for /f "tokens=2*" %%S in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuil
 )
 
 endlocal
-if not "%1"=="-nopause" pause
+:eoscript
+if not "%NOPAUSE%"=="-nopause" pause
