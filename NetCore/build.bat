9b9a9b14aec56f886514c234eab155eb803150d2
Maksim Volkau <max.volkov@gmail.com>
2018-03-18 22:06:21 +0300

    released: DI.MS.DI v2.0.0-preview-03
    --HG--
    branch : dev

--- NetCore/build.bat
+++ NetCore/build.bat
@@ -6,6 +6,7 @@ dotnet build
 
 dotnet test ".\test\DryIoc.NetCore.UnitTests"
 dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests"
+rem dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests.v1.1"
 
 dotnet pack ".\src\DryIoc.Microsoft.DependencyInjection" -c Release -o "..\bin\NuGetPackages"
 
