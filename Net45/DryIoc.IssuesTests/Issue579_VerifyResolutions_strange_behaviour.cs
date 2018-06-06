4ddaa3d51df33a00ea7ec5519e34ecf4210148b0
Maksim Volkau <max.volkov@gmail.com>
2018-06-06 20:28:17 +0300

    wip: added test for #579
    --HG--
    branch : dev

---
+++ Net45/DryIoc.IssuesTests/Issue579_VerifyResolutions_strange_behaviour.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class Issue579_VerifyResolutions_strange_behaviour
+    {
+        [Test]
+        public void Test()
+        {
+            var container = new Container(rules => rules.WithoutThrowIfDependencyHasShorterReuseLifespan());
+            container.Register<FiosEntities>(Reuse.InWebRequest, setup: Setup.With(openResolutionScope: true));
+            var result = container.Validate();
+
+
+        }
+    }
+
+    public class FiosEntities { }
+}
