771b9500cb4b9a0df263fb5bcd35e96b4a23745e
dadhi <max.volkov@gmail.com>
2018-09-28 14:50:33 +0000

    updated: DryIoc nuspecs for v3.1.0-preview-01

--- DryIoc/Properties/AssemblyInfo.cs
+++ DryIoc/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("DryIoc")]
 [assembly: AssemblyDescription("DryIoc is fast, small, full-featured IoC Container for .NET")]
 [assembly: AssemblyCopyright("Copyright © 2013 Maksim Volkau")]
 
-[assembly: AssemblyVersion             ("3.0.2")]
-[assembly: AssemblyFileVersion         ("3.0.2")]
-[assembly: AssemblyInformationalVersion("3.0.2")]
+[assembly: AssemblyVersion             ("3.1.0")]
+[assembly: AssemblyFileVersion         ("3.1.0")]
+[assembly: AssemblyInformationalVersion("3.1.0-preview-01")]
 
 [assembly: InternalsVisibleTo("DryIoc.IssuesTests")]
