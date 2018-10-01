1dcf62179d7985019f479470a6dd81414f63b044
dadhi <max.volkov@gmail.com>
2018-10-01 08:00:14 +0000

    Compileable DryIoc except PCls with Ported..cs integrated into Container.cs

---
+++ NetCore/src/DryIoc.UnitTests/RegisterResolveTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DryIoc.NetCore.UnitTests
+{
+    [TestFixture]
+    public class RegisterResolveTests
+    { 
+        [Test]
+        public void Register_and_Resolve_should_work()
+        {
+            var container = new Container(Rules.Default.WithAutoConcreteTypeResolution());
+
+            var a = container.Resolve<A>();
+
+            Assert.NotNull(a);
+        }
+
+        public class A {}
+    }
+}
---
+++ NetCore/src/Samples/DryIoc.AspNetCore.Sample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+using Microsoft.AspNetCore.Mvc;
+using DryIoc.AspNetCore.Sample.Models.Home;
+using DryIoc.AspNetCore.Sample.Services;
+
+namespace DryIoc.AspNetCore.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IScopedService Scoped { get; }
+        public ITransientService Transient { get; }
+        public IExportedService Imported { get; }
+
+        public HomeController(IScopedService scoped, ITransientService transient, IExportedService imported = null)
+        {
+            Scoped = scoped;
+            Transient = transient;
+            Imported = imported;
+        }
+
+        [Route("/")]
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            var model = new HomeModel
+            {
+                Message = $"Transient services are different: {Transient != Scoped.Transient}; "
+                        + $"Singletons are the same: {Transient.Singleton == Scoped.Singleton}; "
+                        + $"Exported service is successfully imported {Imported != null}"
+            };
+
+            return View(model);
+        }
+
+        // just for route test
+        [Route("home/greet/{username}")]
+        public IActionResult Greet(string username)
+        {
+            return Ok("Hi " + username);
+        }
+    }
+}
