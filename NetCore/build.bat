ef315495271092a4095d2632378ba0054053ff3f
dadhi <max.volkov@gmail.com>
2018-10-01 20:27:00 +0000

    added back a src, test, samples folder structure

--- NetCore/build.bat
+++ NetCore/build.bat
@@ -6,7 +6,7 @@ if %ERRORLEVEL% neq 0 call :error "dotnet restore"
 dotnet build
 if %ERRORLEVEL% neq 0 call :error "dotnet build"
 
-dotnet test ".\src\DryIoc.UnitTests"
+dotnet test ".\test\DryIoc.UnitTests"
 REM dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests"
 REM dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests.v1.1"
 if %ERRORLEVEL% neq 0 call :error "dotnet test"
