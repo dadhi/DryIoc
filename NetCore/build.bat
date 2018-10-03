00eb304c8f7cfc47a0914f6c6de4b6fa733d5069
dadhi <max.volkov@gmail.com>
2018-10-03 11:00:33 +0000

    added: test runner with coverage

--- NetCore/build.bat
+++ NetCore/build.bat
@@ -15,6 +15,39 @@ echo:
 echo:## BUILD IS SUCCESSFUL ##
 echo:
 
+set NUNIT="packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe"
+set OPENCOVER="packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
+set REPORTGEN="packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe"
+set REPORTS=.\CoverageReport
+set COVERAGE=%REPORTS%\Coverage.xml
+if not exist %REPORTS% md %REPORTS% 
+
+set TESTS=.\test\DryIoc.UnitTests\bin\Release\net45\DryIoc.UnitTests.dll^
+    .\test\DryIoc.IssuesTests\bin\Release\net45\DryIoc.IssuesTests.dll^
+    .\test\DryIoc.MefAttributedModel.UnitTests\bin\Release\net45\DryIoc.MefAttributedModel.UnitTests.dll
+
+echo:
+echo:## RUNNING TESTS: %TESTS%
+echo: 
+%OPENCOVER%^
+ -register:path64^
+ -target:%NUNIT%^
+ -targetargs:"%TESTS%"^
+ -filter:"+[*]* -[*Test*]* -[*Docs*]* -[*Moq*]* -[Microsoft*]* -[xunit*]* -[NetCore*]*"^
+ -excludebyattribute:*.ExcludeFromCodeCoverageAttribute^
+ -hideskipped:all^
+ -output:%COVERAGE%
+
+echo:
+echo:## GENERATING "%COVERAGE%" . . .
+echo: 
+%REPORTGEN%^
+ -reports:%COVERAGE%^
+ -targetdir:%REPORTS%^
+ -reporttypes:Html;HtmlSummary;Badges^
+ -assemblyfilters:-*Test*^
+ -classfilters:-DryIoc.Arg
+
 REM dotnet test ".\test\DryIoc.UnitTests"
 REM dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests"
 REM dotnet test ".\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests.v1.1"
