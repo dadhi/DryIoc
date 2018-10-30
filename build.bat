bb27d8c75dba9fbccb867553d753af8bbf0c3951
dadhi <max.volkov@gmail.com>
2018-10-30 16:47:43 +0000

    fixed: Build error: "Illegal characters in the path.." by removing projects starting from DryIoc.Mvc.UnitTests project
    fixed: build.bat not reacting to non-zero error

--- build.bat
+++ build.bat
@@ -8,16 +8,20 @@ set MSB="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuil
 if not exist %MSB% set MSB="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\bin\MSBuild.exe"
 if not exist %MSB% for /f "tokens=4 delims='" %%p IN ('.nuget\nuget.exe restore ^| find "MSBuild auto-detection"') do set MSB="%%p\MSBuild.exe"
 echo:
-echo:## USING MSBUILD: %MSB%
+echo:## Using MSBuild: %MSB%
 echo:
+echo:## Starting: RESTORE and BUILD... ##
+echo: 
 
 rem dotnet clean --verbosity:minimal
 
 rem: Turning Off the $(DevMode) from the Directory.Build.props to alway build an ALL TARGETS
 call %MSB% %SLN% /t:Restore;Rebuild /p:DevMode=false /p:Configuration=Release /verbosity:minimal /fl /flp:LogFile=MSBuild.log
-if %ERRORLEVEL% neq 0 call :error "BUILD;PACK"
+if %ERRORLEVEL% neq 0 goto :error
 echo:
-echo:## RESTORE and BUILD is SUCCESSFUL ##
+echo:## Finished: RESTORE and BUILD ##
+echo:
+echo:## Starting: TESTS... ##
 echo:
 
 dotnet test -c:Release --no-build .\docs\DryIoc.Docs
@@ -25,9 +29,11 @@ dotnet test -c:Release --no-build .\test\DryIoc.UnitTests
 dotnet test -c:Release --no-build .\test\DryIoc.IssuesTests
 dotnet test -c:Release --no-build .\test\DryIoc.MefAttributedModel.UnitTests
 dotnet test -c:Release --no-build .\test\DryIoc.Microsoft.DependencyInjection.Specification.Tests
-if %ERRORLEVEL% neq 0 call :error "TESTS"
+if %ERRORLEVEL% neq 0 goto :error
 echo:
-echo:## TESTS are SUCCESSFUL ##
+echo:## Finished: TESTS ##
+echo:
+echo:## Starting: PACKAGING... ##
 echo:
 
 dotnet pack -c:Release --no-build -o:..\..\.dist\packages .\src\DryIoc\DryIoc.csproj
@@ -40,15 +46,14 @@ if %ERRORLEVEL% neq 0 call :error "PACKAGING"
 call BuildScripts\NugetPack.bat
 if %ERRORLEVEL% neq 0 call :error "PACKAGING SOURCE PACKAGES"
 echo:
-echo:## PACKAGING is SUCCESSFUL ##
+echo:## Finished: PACKAGING ##
 echo:
-
-echo:## ALL done SUCCESSFULLY ##
+echo:## Finished: ALL ##
 echo:
 exit /b 0
 
 :error
 echo:
-echo:## %1 FAILED WITH ERROR: %ERRORLEVEL%
+echo:## :-( Failed with ERROR: %ERRORLEVEL%
 echo:
 exit /b %ERRORLEVEL%
