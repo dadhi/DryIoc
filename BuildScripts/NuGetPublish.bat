37f4edae345d747d8872c82240ebe9826e2ea671
dadhi <max.volkov@gmail.com>
2019-03-30 23:24:18 +0300

    updated VersionHistory

--- BuildScripts/NuGetPublish.bat
+++ BuildScripts/NuGetPublish.bat
@@ -4,13 +4,15 @@ set PACKAGES=..\.dist\packages
 set SOURCE=https://api.nuget.org/v3/index.json
 set /p APIKEY=<"..\ApiKey.txt"
 
-dotnet nuget push "%PACKAGES%\DryIoc.dll.4.0.1.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\DryIoc.4.0.1.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\DryIoc.Internal.4.0.1.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.dll.4.0.2.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.4.0.2.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.Internal.4.0.2.nupkg" -k %APIKEY% -s %SOURCE%
 
 rem dotnet nuget push "%PACKAGES%\DryIocZero.4.1.0.nupkg" -k %APIKEY% -s %SOURCE%
 
-dotnet nuget push "%PACKAGES%\DryIoc.Microsoft.DependencyInjection.3.0.1.nupkg" -k %APIKEY% -s %SOURCE%
+rem dotnet nuget push "%PACKAGES%\DryIoc.Microsoft.DependencyInjection.3.0.1.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.Microsoft.DependencyInjection.src.3.0.1.nupkg" -k %APIKEY% -s %SOURCE%
+
 rem dotnet nuget push "%PACKAGES%\DryIoc.Microsoft.Hosting.1.0.0-preview-01.nupkg" -k %APIKEY% -s %SOURCE%
 
 rem dotnet nuget push "%PACKAGES%\DryIocAttributes.dll.5.0.0.nupkg" -k %APIKEY% -s %SOURCE%
