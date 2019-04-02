ce04ab97ed4af5c6e24f19892b9d6c926a10c833
Riccardo De Agostini <riccardo@tenacom.it>
2019-04-02 12:47:15 +0200

    Add test for issue #107.

---
+++ test/DryIoc.IssuesTests/GHIssue107_Resolve_still_caches_args_values_when_using_instance_factory_method.cs
@@ -0,0 +1,73 @@
+using System;
+using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class GHIssue107_Resolve_still_caches_args_values_when_using_instance_factory_method
+    {
+        [Test]
+        public void Resolve_shall_not_cache_args_values()
+        {
+            var c = new Container(rules => rules.WithDefaultReuse(Reuse.Singleton));
+            c.Register<NameFactory>();
+            c.Register<IName>(made: Made.Of(r => ServiceInfo.Of<NameFactory>(), f => f.CreateName(Arg.Of<string>())));
+
+            c.Register<IAlpha, Alpha>();
+            c.Register<IBravo, Bravo>();
+
+            c.Resolve<IAlpha>(args: new object[] { "Alice" });
+            Assert.AreEqual("Brenda", c.Resolve<IBravo>(args: new object[] { "Brenda" }).Name.Name);
+        }
+
+        private interface IName
+        {
+            string Name { get; }
+        }
+
+        private class NameFactory
+        {
+            private class NameImpl : IName
+            {
+                internal NameImpl(string name)
+                {
+                    Name = name;
+                }
+
+                public string Name { get; }
+            }
+
+            public IName CreateName(string name) => new NameImpl(name);
+        }
+
+        private interface IAlpha
+        {
+            IName Name { get; }
+        }
+
+        private interface IBravo
+        {
+            IName Name { get; }
+        }
+
+        private class Alpha : IAlpha
+        {
+            public Alpha(IName name)
+            {
+                Name = name;
+            }
+
+            public IName Name { get; }
+        }
+
+        private class Bravo : IBravo
+        {
+            public Bravo(IName name)
+            {
+                Name = name;
+            }
+
+            public IName Name { get; }
+        }
+    }
+}
