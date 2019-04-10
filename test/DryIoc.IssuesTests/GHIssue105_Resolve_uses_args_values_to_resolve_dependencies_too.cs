d8187f0ad0d44a1a7576ed5c113c9ecf84ca324c
dadhi <max.volkov@gmail.com>
2019-04-10 16:25:05 +0300

    unignore test for #105 - passing just fine

--- test/DryIoc.IssuesTests/GHIssue105_Resolve_uses_args_values_to_resolve_dependencies_too.cs
+++ test/DryIoc.IssuesTests/GHIssue105_Resolve_uses_args_values_to_resolve_dependencies_too.cs
@@ -6,18 +6,17 @@ namespace DryIoc.IssuesTests
     [TestFixture]
     public class GHIssue105_Resolve_uses_args_values_to_resolve_dependencies_too
     {
-        [Test][Ignore("fix me")]
+        [Test]//[Ignore("fix me")]
         public void Resolve_shall_not_use_args_to_resolve_dependencies()
         {
             var c = new Container();
             c.Register<IInterface1, Implementation1>();
             c.Register<IInterface2, Implementation2>();
 
-            Assert.Throws<ContainerException>(() =>
-            {
-                var id = Guid.NewGuid();
-                c.Resolve(typeof(IInterface2), new object[] { id });
-            });
+            var id = Guid.NewGuid();
+            var i2 = c.Resolve<IInterface2>(new object[] { id });
+
+            Assert.AreEqual(i2.Id, i2.Interface1.Id);
         }
 
         private class Implementation1 : IInterface1
@@ -32,14 +31,22 @@ namespace DryIoc.IssuesTests
         private class Implementation2 : IInterface2
         {
             public Guid Id { get; }
+            public IInterface1 Interface1 { get; }
+
             public Implementation2(Guid id, IInterface1 interface1)
             {
                 Id = id;
+                Interface1 = interface1;
             }
         }
 
-        private interface IInterface1 { }
+        private interface IInterface1 {
+            Guid Id { get; }
+        }
 
-        private interface IInterface2 { }
+        private interface IInterface2 {
+            Guid Id { get; }
+            IInterface1 Interface1 { get; }
+        }
     }
 }
