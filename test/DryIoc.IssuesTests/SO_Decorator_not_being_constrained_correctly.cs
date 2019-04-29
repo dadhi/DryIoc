a4b0a852b72d12b722d13a02e23d6d26d5c911ea
dadhi <max.volkov@gmail.com>
2019-04-29 19:27:37 +0300

    added: more context into test from the SO question: https://stackoverflow.com/questions/55749917/decorator-not-being-constrained-correctly

--- test/DryIoc.IssuesTests/SO_Decorator_not_being_constrained_correctly.cs
+++ test/DryIoc.IssuesTests/SO_Decorator_not_being_constrained_correctly.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 using NUnit.Framework;
 
 namespace DryIoc.IssuesTests
@@ -5,5 +9,45 @@ namespace DryIoc.IssuesTests
     [TestFixture]
     public class SO_Decorator_not_being_constrained_correctly
     {
+        [Test]
+        public void Test()
+        {
+            var c = new Container();
+
+            c.Register(typeof(IRequestHandler<,>), typeof(RetryOnConcurrencyRequestHandlerDecorator<,>), setup: Setup.Decorator);
+        }
+    }
+
+    public class RetryOnConcurrencyRequestHandlerDecorator<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, IRetryOnConflict
+    {
+        private readonly IConcurrencyRetryPolicy retryPolicy;
+        private readonly IRequestHandler<TRequest, TResponse> innerHandler;
+
+        public RetryOnConcurrencyRequestHandlerDecorator(IRequestHandler<TRequest, TResponse> innerHandler, IConcurrencyRetryPolicy retryPolicy)
+        {
+            this.innerHandler = innerHandler;
+            this.retryPolicy = retryPolicy;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            return retryPolicy.Execute(() => innerHandler.Handle(request, cancellationToken));
+        }
+    }
+
+    public interface IRetryOnConflict
+    {
+    }
+
+    public class GetSaleRegistration : IRequest<SaleRegistration>
+    {
+    }
+
+    public class SaleRegistration { }
+
+    public interface IConcurrencyRetryPolicy
+    {
+        Task<TResponse> Execute<TResponse>(Func<Task<TResponse>> func);
     }
 }
