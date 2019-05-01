9361e9f005d9c5640cafdce70bf0cc8879de3ae3
dadhi <max.volkov@gmail.com>
2019-05-01 19:58:07 +0300

    pushing the v4.0.4

--- BuildScripts/NuGetPublish.bat
+++ BuildScripts/NuGetPublish.bat
@@ -4,9 +4,9 @@ set PACKAGES=..\.dist\packages
 set SOURCE=https://api.nuget.org/v3/index.json
 set /p APIKEY=<"..\ApiKey.txt"
 
-dotnet nuget push "%PACKAGES%\DryIoc.dll.4.0.3.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\DryIoc.4.0.3.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\DryIoc.Internal.4.0.3.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.dll.4.0.4.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.4.0.4.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.Internal.4.0.4.nupkg" -k %APIKEY% -s %SOURCE%
 
 rem dotnet nuget push "%PACKAGES%\DryIocZero.4.1.0.nupkg" -k %APIKEY% -s %SOURCE%
 
