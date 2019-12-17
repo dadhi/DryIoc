8a975160456803e3d0aab4a28f2336a559447613
dadhi <max.volkov@gmail.com>
2019-12-17 13:48:38 +0300

    using FactoryDelegate together with new Func-based injectable RegisterDelegate - no need for the separate FooFactory here

--- test/DryIoc.IssuesTests/GHIssue191_Optional_IResolverContext_argument_in_Func_of_service.cs
+++ test/DryIoc.IssuesTests/GHIssue191_Optional_IResolverContext_argument_in_Func_of_service.cs
@@ -93,6 +93,60 @@ namespace DryIoc.IssuesTests
             }
         }
 
+        [Test]
+        public void Main_test_with_RegisterDelegate_and_strongly_typed_FactoryDelegate()
+        {
+            var c = new Container();
+
+            c.Register<IFoo, Foo>(Reuse.Scoped);
+            c.Register<IFoo, FooDecorator>(Reuse.Scoped, setup: Setup.Decorator);
+
+            var _scopes = ImHashMap<string, IResolverContext>.Empty;
+            c.RegisterDelegate<IResolverContext, FactoryDelegate<Func<string, IFoo>>, string, IFoo>(
+                (ctx, decorateeFactory, address) =>
+                {
+                    // fancy ensuring that we have a single new Scope created and stored
+                    var scopeEntry = _scopes.GetEntryOrDefault(address);
+                    if (scopeEntry == null)
+                    {
+                        Ref.Swap(ref _scopes, address, (x, a) => x.AddEntryOrKeep(a));
+                        scopeEntry = _scopes.GetEntryOrDefault(address);
+                    }
+
+                    lock (scopeEntry)
+                        if (scopeEntry.Value == null || scopeEntry.Value.IsDisposed)
+                            scopeEntry.Value = ctx.OpenScope(address);
+
+                    return decorateeFactory(scopeEntry.Value).Invoke(address);
+                },
+
+                setup: Setup.Decorator);
+
+
+            using (var scope = c.OpenScope())
+            {
+                var foo1 = scope.Resolve<IFoo>(new object[] { "1" });
+                var foo2 = scope.Resolve<IFoo>(new object[] { "2" });
+
+                // Test that each IFoo is a different instance.
+                Assert.AreNotSame(foo1, foo2);
+
+                // That that I get the same instance when provided the same address while the named scope (address)
+                // has not been disposed.
+                var foo11 = scope.Resolve<IFoo>(new object[] { "1" });
+                Assert.AreSame(foo1, foo11);
+
+                var foo22 = scope.Resolve<IFoo>(new object[] { "2" });
+                Assert.AreSame(foo2, foo22);
+
+                // Test that after disposing FooDecorator, the named scope is closed and I get
+                // a new instance of foo for address = "1". 
+                foo1.Dispose();
+                var foo111 = scope.Resolve<IFoo>(new object[] { "1" });
+                Assert.AreNotSame(foo1, foo111);
+            }
+        }
+
         public interface IFoo : IDisposable
         {
             void DoSomething();
