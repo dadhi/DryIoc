530eb31915388bbfafd3ec2f81a83716ffc2afa3
dadhi <max.volkov@gmail.com>
2019-12-17 12:01:53 +0300

    FactoryDelegate<T> wrapper support #191

--- test/DryIoc.IssuesTests/GHIssue191_Optional_IResolverContext_argument_in_Func_of_service.cs
+++ test/DryIoc.IssuesTests/GHIssue191_Optional_IResolverContext_argument_in_Func_of_service.cs
@@ -57,6 +57,42 @@ namespace DryIoc.IssuesTests
             }
         }
 
+        [Test]
+        public void Main_test_with_strongly_typed_FactoryDelegate()
+        {
+            var c = new Container();
+
+            c.Register<IFoo, Foo>(Reuse.Scoped);
+            c.Register<IFoo, FooDecorator>(Reuse.Scoped, setup: Setup.Decorator);
+            c.Register<FooFactory>(Reuse.Scoped);
+            c.Register<IFoo>(Made.Of(_ => ServiceInfo.Of<FooFactory>(),
+                    f => f.GetOrCreate2(Arg.Of<IResolverContext>(), Arg.Of<FactoryDelegate<Func<string, IFoo>>>(), Arg.Of<string>())),
+                setup: Setup.Decorator);
+
+            using (var scope = c.OpenScope())
+            {
+                var foo1 = scope.Resolve<IFoo>(new object[] { "1" });
+                var foo2 = scope.Resolve<IFoo>(new object[] { "2" });
+
+                // Test that each IFoo is a different instance.
+                Assert.AreNotSame(foo1, foo2);
+
+                // That that I get the same instance when provided the same address while the named scope (address)
+                // has not been disposed.
+                var foo11 = scope.Resolve<IFoo>(new object[] { "1" });
+                Assert.AreSame(foo1, foo11);
+
+                var foo22 = scope.Resolve<IFoo>(new object[] { "2" });
+                Assert.AreSame(foo2, foo22);
+
+                // Test that after disposing FooDecorator, the named scope is closed and I get
+                // a new instance of foo for address = "1". 
+                foo1.Dispose();
+                var foo111 = scope.Resolve<IFoo>(new object[] { "1" });
+                Assert.AreNotSame(foo1, foo111);
+            }
+        }
+
         public interface IFoo : IDisposable
         {
             void DoSomething();
@@ -108,6 +144,23 @@ namespace DryIoc.IssuesTests
                         scopeEntry.Value = ctx.OpenScope(address);
 
                 return ((Func<string, IFoo>)fooFactory(scopeEntry.Value)).Invoke(address);
+            }
+
+            public IFoo GetOrCreate2(IResolverContext ctx, FactoryDelegate<Func<string, IFoo>> fooFactory, string address)
+            {
+                // fancy ensuring that we have a single new Scope created and stored
+                var scopeEntry = _scopes.GetEntryOrDefault(address);
+                if (scopeEntry == null)
+                {
+                    Ref.Swap(ref _scopes, address, (x, a) => x.AddEntryOrKeep(a));
+                    scopeEntry = _scopes.GetEntryOrDefault(address);
+                }
+
+                lock (scopeEntry)
+                    if (scopeEntry.Value == null || scopeEntry.Value.IsDisposed)
+                        scopeEntry.Value = ctx.OpenScope(address);
+
+                return fooFactory(scopeEntry.Value).Invoke(address);
             }
 
             // Closes all scopes - Note: alternatively we may add scope tracking in parent and rely on parent scope disposal
