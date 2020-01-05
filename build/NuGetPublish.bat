f73289476ec91b890820363ee342abc970caa9e6
dadhi <max.volkov@gmail.com>
2020-01-05 22:06:49 +0300

    added missing reslease notes

--- build/NuGetPublish.bat
+++ build/NuGetPublish.bat
@@ -2,11 +2,11 @@
 
 set PACKAGES=..\.dist\packages
 set SOURCE=https://api.nuget.org/v3/index.json
-set /p APIKEY=<"..\ApiKey.txt"
+set /p APIKEY=<"..\..\ApiKey.txt"
 
-dotnet nuget push "%PACKAGES%\DryIoc.dll.4.1.0-preview-03.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\DryIoc.4.1.0-preview-03.nupkg" -k %APIKEY% -s %SOURCE%
-dotnet nuget push "%PACKAGES%\DryIoc.Internal.4.1.0-preview-03.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.dll.4.1.0-preview-04.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.4.1.0-preview-04.nupkg" -k %APIKEY% -s %SOURCE%
+dotnet nuget push "%PACKAGES%\DryIoc.Internal.4.1.0-preview-04.nupkg" -k %APIKEY% -s %SOURCE%
 
 rem dotnet nuget push "%PACKAGES%\DryIocZero.4.1.0.nupkg" -k %APIKEY% -s %SOURCE%
 
