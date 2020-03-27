412a322072f2dbb68a3a5b81e55a296fbecabf0b
m.volkau <maksim.volkau@adform.com>
2020-03-27 14:04:55 +0300

    Trying #242 and #245

--- src/DryIoc/Container.cs
+++ src/DryIoc/Container.cs
@@ -10086,6 +10086,9 @@ namespace DryIoc
                 }
             }
 
+            if (rules.UsedForValidation) 
+                return Default(request.GetActualServiceType());
+
             var ctor = ctorOrMethod as ConstructorInfo;
             Expression serviceExpr;
             if (arg0 == null)
