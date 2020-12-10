73bb0fb6f3764ca99234c81b3a4eba22dd89b620
maximv <maximv@cynet.com>
2020-12-10 23:27:14 +0300

    removing using var

--- test/DryIoc.IssuesTests/GHIssue344_Scope_is_disposed_before_parent_when_using_facade.cs
+++ test/DryIoc.IssuesTests/GHIssue344_Scope_is_disposed_before_parent_when_using_facade.cs
@@ -14,22 +14,23 @@ namespace DryIoc.IssuesTests
             root.Register<MyDisposable>(Reuse.Scoped);
 
             // this happens within the DryIoc-MsDI adapter when ASP handles an http request 
-            using var scope = root.OpenScope();
-
-            // some service is resolved, that requires the disposable
-            scope.Resolve<MyDisposable>();
-
-            // another service is resolved, that depends on `IContainer` which it will receive from the context given to ASP
-            // now, `container == ctx` which should be fine since the `WithCurrentScope` call returns an instance of `Container`
-            var container = scope.Resolve<IContainer>();
-
-            // the service now creates a facade for and works with it. After it's done, the facade is disposed.
-            using (container.CreateFacade())
+            using (var scope = root.OpenScope())
             {
-                // do anything at all, it doesn't matter
-            }
+                // some service is resolved, that requires the disposable
+                scope.Resolve<MyDisposable>();
 
-            scope.Resolve<MyDisposable>(); // should not throw that the scope is disposed
+                // another service is resolved, that depends on `IContainer` which it will receive from the context given to ASP
+                // now, `container == ctx` which should be fine since the `WithCurrentScope` call returns an instance of `Container`
+                var container = scope.Resolve<IContainer>();
+
+                // the service now creates a facade for and works with it. After it's done, the facade is disposed.
+                using (container.CreateFacade())
+                {
+                    // do anything at all, it doesn't matter
+                }
+
+                scope.Resolve<MyDisposable>(); // should not throw that the scope is disposed
+            }
         }
 
         class MyDisposable : IDisposable
