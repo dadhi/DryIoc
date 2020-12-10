9d193b044a2c6012be6ed4a4c47e4a9461aa553b
maximv <maximv@cynet.com>
2020-12-10 17:00:39 +0300

    added: more cases to #350

--- test/DryIoc.IssuesTests/GHIssue350_Wrong_scoped_resolve.cs
+++ test/DryIoc.IssuesTests/GHIssue350_Wrong_scoped_resolve.cs
@@ -26,13 +26,79 @@ namespace DryIoc.IssuesTests
 
             using (var scope2 = container.OpenScope("SecondScope"))
             {
-                // throws: DryIoc.ContainerException : code: Error.NoMatchedScopeFound; 
-                // message: Unable to find matching scope with name "FirstScope" starting from 
-                // the current scope {Name=SecondScope}.
-                // var aExpr = scope2.Resolve<LambdaExpression>(typeof(A));
-
                 var a = scope2.Resolve<A>();
                 Assert.IsInstanceOf<S2>(a.B.S);
+            }
+        }
+
+        [Test]
+        public void TheBug_without_MS_DI_rules()
+        {
+            var container = new Container(rules => rules
+                // .WithMicrosoftDependencyInjectionRules()
+                .WithFuncAndLazyWithoutRegistration());
+
+            container.Register<A>();
+            container.Register<B>();
+            container.Register<S, S1>(Reuse.ScopedTo("FirstScope"));
+            container.Register<S, S2>(Reuse.ScopedTo("SecondScope"));
+
+            using (var scope1 = container.OpenScope("FirstScope"))
+            {
+                var a = scope1.Resolve<A>();
+                Assert.IsInstanceOf<S1>(a.B.S);
+            }
+
+            using (var scope2 = container.OpenScope("SecondScope"))
+            {
+                var a = scope2.Resolve<A>();
+                Assert.IsInstanceOf<S2>(a.B.S);
+            }
+        }
+
+        [Test]
+        public void TheBug_simplified()
+        {
+            var container = new Container(rules => rules
+                .WithMicrosoftDependencyInjectionRules()
+                .WithFuncAndLazyWithoutRegistration());
+
+            container.Register<S, S1>(Reuse.ScopedTo("FirstScope"));
+            container.Register<S, S2>(Reuse.ScopedTo("SecondScope"));
+
+            using (var scope1 = container.OpenScope("FirstScope"))
+            {
+                var a = scope1.Resolve<S>();
+                Assert.IsInstanceOf<S1>(a);
+            }
+
+            using (var scope2 = container.OpenScope("SecondScope"))
+            {
+                var a = scope2.Resolve<S>();
+                Assert.IsInstanceOf<S2>(a);
+            }
+        }
+
+        [Test]
+        public void TheBug_simplified_without_MS_DI_rules()
+        {
+            var container = new Container(rules => rules
+                // .WithMicrosoftDependencyInjectionRules()
+                .WithFuncAndLazyWithoutRegistration());
+
+            container.Register<S, S1>(Reuse.ScopedTo("FirstScope"));
+            container.Register<S, S2>(Reuse.ScopedTo("SecondScope"));
+
+            using (var scope1 = container.OpenScope("FirstScope"))
+            {
+                var a = scope1.Resolve<S>();
+                Assert.IsInstanceOf<S1>(a);
+            }
+
+            using (var scope2 = container.OpenScope("SecondScope"))
+            {
+                var a = scope2.Resolve<S>();
+                Assert.IsInstanceOf<S2>(a);
             }
         }
 
