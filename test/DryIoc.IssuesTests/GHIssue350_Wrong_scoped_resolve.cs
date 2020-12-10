53c0f3beb12925b34b0f4846f66726dd86cc5b1d
maximv <maximv@cynet.com>
2020-12-10 17:09:32 +0300

    added the test for #352

--- test/DryIoc.IssuesTests/GHIssue350_Wrong_scoped_resolve.cs
+++ test/DryIoc.IssuesTests/GHIssue350_Wrong_scoped_resolve.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using FastExpressionCompiler.LightExpression;
 
 namespace DryIoc.IssuesTests
 {
--- test/DryIoc.IssuesTests/GHIssue352_Consider_resolving_the_variance_compatible_open_generic_the_same_as_for_collection_of_open_generics.cs
+++ test/DryIoc.IssuesTests/GHIssue352_Consider_resolving_the_variance_compatible_open_generic_the_same_as_for_collection_of_open_generics.cs
@@ -6,7 +6,7 @@ using System.Threading;
 namespace DryIoc.IssuesTests
 {
     [TestFixture]
-    public class GHIssue____DryIoc_Message_SO
+    public class GHIssue352_Consider_resolving_the_variance_compatible_open_generic_the_same_as_for_collection_of_open_generics 
     {
         [Test, Ignore("fixme")]
         public void Contravariant_handler_should_be_Resolved_the_same_as_for_ResolveMany()
@@ -17,16 +17,18 @@ namespace DryIoc.IssuesTests
             container.RegisterMany(new[] { typeof(GetInformationHandler).Assembly }, Registrator.Interfaces, made: PropertiesAndFields.Auto);
             container.Register<MessageMediator>();
 
+            // This works.
             var handlers = container.Resolve<IMessageHandler<PermissionedGetInformationRequest, DataIWantView>[]>();
             Assert.AreEqual(1, handlers.Length);
-
             var result = handlers[0].Handle(new PermissionedGetInformationRequest(), default);
             Assert.IsNotNull(result);
 
+            // But this does not work!
             var handler = container.Resolve<IMessageHandler<PermissionedGetInformationRequest, DataIWantView>>();
             result = handler.Handle(new PermissionedGetInformationRequest(), default);
             Assert.IsNotNull(result);
 
+            // and this does not work!
             var m = container.Resolve<MessageMediator>();
             var task = m.Send<PermissionedGetInformationRequest, DataIWantView>(new PermissionedGetInformationRequest(), default);
             Assert.IsNotNull(task);
