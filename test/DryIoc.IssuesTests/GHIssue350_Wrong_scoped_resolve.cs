67404b917fd62fbdb4b4185c8d0b286c110b1a64
maximv <maximv@cynet.com>
2020-12-11 13:30:18 +0300

    added the new tests for #350

--- test/DryIoc.IssuesTests/GHIssue350_Wrong_scoped_resolve.cs
+++ test/DryIoc.IssuesTests/GHIssue350_Wrong_scoped_resolve.cs
@@ -5,6 +5,67 @@ namespace DryIoc.IssuesTests
     [TestFixture]
     public class GHIssue350_Wrong_scoped_resolve
     {
+        [Test, Ignore("todo: fixme")]
+        public void TheBug_2()
+        {
+            var container = new Container(rules => rules
+                .WithMicrosoftDependencyInjectionRules()
+                .WithFuncAndLazyWithoutRegistration());
+
+            container.Register<A>();
+            container.Register<S, S1>(Reuse.ScopedTo("FirstScope"));
+            container.Register<B, B2>(Reuse.ScopedTo("SecondScope"));
+            container.Register<B, B1>();
+
+            using (var scope = container.OpenScope("FirstScope"))
+            {
+                // no issue
+                var a = scope.Resolve<A>();
+                Assert.IsInstanceOf<B1>(a.B);
+                Assert.IsInstanceOf<S1>(a.B.S);
+            }
+
+            using (var context = container.OpenScope("SecondScope"))
+            {
+               //DryIoc.ContainerException : code: Error.NoMatchedScopeFound;
+               //message: Unable to find matching scope with name "FirstScope" starting from the current scope {Name=SecondScope}.
+                var a = context.Resolve<A>();
+                Assert.IsInstanceOf<B2>(a.B);
+                Assert.IsNull(a.B.S);
+            }
+        }
+
+        [Test]
+        public void TheBug_2_change_resgitration_order()
+        {
+            var container = new Container(rules => rules
+                .WithMicrosoftDependencyInjectionRules()
+                .WithFuncAndLazyWithoutRegistration());
+
+            container.Register<A>();
+            container.Register<B, B1>();
+            container.Register<S, S1>(Reuse.ScopedTo("FirstScope"));
+            container.Register<B, B2>(Reuse.ScopedTo("SecondScope"));
+
+            using (var scope = container.OpenScope("FirstScope"))
+            {
+                // no issue
+                var a = scope.Resolve<A>();
+                Assert.IsInstanceOf<B1>(a.B);
+                Assert.IsInstanceOf<S1>(a.B.S);
+            }
+
+            using (var context = container.OpenScope("SecondScope"))
+            {
+               //DryIoc.ContainerException : code: Error.NoMatchedScopeFound;
+               //message: Unable to find matching scope with name "FirstScope" starting from the current scope {Name=SecondScope}.
+                var a = context.Resolve<A>();
+                Assert.IsInstanceOf<B2>(a.B);
+                Assert.IsNull(a.B.S);
+            }
+        }
+
+
         [Test]
         public void TheBug()
         {
@@ -112,6 +173,9 @@ namespace DryIoc.IssuesTests
             public S S { get; }
             public B(S s) => S = s;
         }
+
+        class B1 : B { public B1(S s) : base(s) {} }
+        class B2 : B { public B2() : base(null) {} }
 
         interface S {}
         class S1 : S {}
