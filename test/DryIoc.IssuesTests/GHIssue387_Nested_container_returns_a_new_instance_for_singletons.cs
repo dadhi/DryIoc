f8855872a775ebfca72594b88541282c98e768b9
dadhi <max.volkov@gmail.com>
2021-03-26 12:31:14 +0300

    added the test for the #387

---
+++ test/DryIoc.IssuesTests/GHIssue387_Nested_container_returns_a_new_instance_for_singletons.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class GHIssue387_Nested_container_returns_a_new_instance_for_singletons
+    {
+        [Test]
+        public void Test1()
+        {
+            var c = new Container(Rules.MicrosoftDependencyInjectionRules);
+
+            c.Register<IMemoryCache, MemCache>(Reuse.Singleton);
+
+            IMemoryCache cache1 = null;
+
+            // cache1 = c.Resolve<IMemoryCache>();
+
+            using (var nested = c.CreateChild())
+            {
+                using (var scope = nested.OpenScope())
+                {
+                    cache1 = scope.Resolve<IMemoryCache>();
+                    var cache2 = scope.Resolve<IMemoryCache>();
+
+                    Assert.AreSame(cache2, cache1);
+                }
+
+                var cache3 = nested.Resolve<IMemoryCache>();
+                Assert.AreSame(cache3, cache1);
+            }
+
+            // Uncomment the line above for this assert to pass.
+            // var cache4 = c.Resolve<IMemoryCache>();
+            // Assert.AreSame(cache4, cache1);
+        }
+
+        interface IMemoryCache {}
+        class MemCache : IMemoryCache {}
+    }
+}
