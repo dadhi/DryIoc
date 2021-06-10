dc753838bf18f2a203a4a7630df9d16197b3049f
dadhi <max.volkov@gmail.com>
2021-06-10 19:50:55 +0300

    correcting api docs

--- src/DryIoc/Container.cs
+++ src/DryIoc/Container.cs
@@ -12917,11 +12917,11 @@ namespace DryIoc
     /// <summary>Policy to handle unresolved service.</summary>
     public enum IfUnresolved
     {
-        /// <summary>If service is unresolved for whatever means, it will throw the respective exception.</summary>
+        /// <summary>If service is unresolved for whatever means, the Resolve will throw the respective exception.</summary>
         Throw,
-        /// <summary>If service is unresolved for whatever means, it will return default(serviceType) value.</summary>
+        /// <summary>If service is unresolved for whatever means, the Resolve will return the default value.</summary>
         ReturnDefault,
-        /// <summary>If service is not registered, then it will return default, for other errors it will throw.</summary>
+        /// <summary>If service is not registered, then the Resolve will return the default value, for the other errors it will throw.</summary>
         ReturnDefaultIfNotRegistered,
     }
 
