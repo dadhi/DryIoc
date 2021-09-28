a3ac423f08481925cc71be23f09054788a3e4814
dadhi <max.volkov@gmail.com>
2021-09-29 00:48:11 +0300

    added test for #417

---
+++ test/DryIoc.IssuesTests/GHIssue417_Performance_degradation_with_dymanic_registrations_in_v4_compared_to_v2.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+using System.ComponentModel.Composition;
+using DryIoc.MefAttributedModel;
+
+namespace DryIoc.IssuesTests
+{
+    [TestFixture]
+    public class GHIssue417_Performance_degradation_with_dynamic_registrations_in_v4_compared_to_v2
+    {
+        [Test]
+        public void SlowTest()
+        {
+            var container = new Container().WithMef();
+
+            container.Register<IService, MyService>();
+
+            var x = new Slow();
+            container.InjectPropertiesAndFields(x);
+
+            Assert.IsInstanceOf<MyService>(x.ImportedServices[0].Value);
+        }
+
+        public class Slow
+        {
+            [Import]
+            public Lazy<IService>[] ImportedServices { get; set; }
+        }
+
+        public interface IService { }
+        public class MyService : IService { }
+    }
+}
