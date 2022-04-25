c8e6b0cda3756b2224bab8b332cf411b901b6cc3
dadhi <max.volkov@gmail.com>
2022-04-25 17:41:27 +0200

    @wip failing thing with placeholder

--- test/DryIoc.IssuesTests/GHIssue101.cs
+++ test/DryIoc.IssuesTests/GHIssue101.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Example;
 
 namespace DryIoc.IssuesTests
 {
@@ -10,8 +11,27 @@ namespace DryIoc.IssuesTests
         {
             var c = new Container();
             c.Register<Example.RuntimeDependencyC>();
-            
+
             var x = c.Resolve<Example.IService>();
+
+            Assert.IsNotNull(x);
+        }
+
+        [Test]
+        public void Emulate_compile_time_generated_example_service_in_runtime()
+        {
+            var container = new Container();
+
+            container.Register<IService, MyService>();
+            container.Register<IDependencyA, DependencyA>();
+
+            container.Register(typeof(DependencyB<>), setup: Setup.With(asResolutionCall: true));
+
+            container.RegisterPlaceholder<RuntimeDependencyC>();
+
+            container.Register<Example.RuntimeDependencyC>();
+
+            var x = container.Resolve<Example.IService>();
 
             Assert.IsNotNull(x);
         }
