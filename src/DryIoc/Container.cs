c57f146f49e672f002e824eb67f4c5b8b83730de
dadhi <max.volkov@gmail.com>
2022-05-07 22:11:05 +0200

    fixing: #180 fixing handling of nullable int with int value and generally default ars with type diff from parameter typ

--- src/DryIoc/Container.cs
+++ src/DryIoc/Container.cs
@@ -6709,7 +6709,7 @@ namespace DryIoc
                         }
 
                         injectedExpr = paramDetails.DefaultValue != null
-                            ? request.Container.GetConstantExpression(paramDetails.DefaultValue)
+                            ? request.Container.GetConstantExpression(paramDetails.DefaultValue, paramRequest.ServiceType)
                             : paramRequest.ServiceType.GetDefaultValueExpression();
                     }
 
@@ -11781,7 +11781,7 @@ namespace DryIoc
                     if (paramDetails.IfUnresolved == IfUnresolved.Throw)
                         return null;
                     injectedExpr = paramDetails.DefaultValue != null
-                        ? container.GetConstantExpression(paramDetails.DefaultValue)
+                        ? container.GetConstantExpression(paramDetails.DefaultValue, paramRequest.ServiceType)
                         : paramRequest.ServiceType.GetDefaultValueExpression();
                 }
 
