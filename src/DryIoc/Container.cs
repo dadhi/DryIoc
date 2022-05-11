e71f4a6dfd8acb54bd883788e56d04af29f6b7f3
dadhi <max.volkov@gmail.com>
2022-05-12 00:54:06 +0200

    fixed: The used instance was not checked in the upper scopes

--- src/DryIoc/Container.cs
+++ src/DryIoc/Container.cs
@@ -13057,15 +13057,18 @@ namespace DryIoc
         /// <summary>Try retrieve the used instance from the scope.</summary>
         public bool TryGetUsed(int hash, Type type, out object used)
         {
-            if (_disposed != 1)
+            if (!_used.IsEmpty)
             {
-                if (!_used.IsEmpty)
-                    return _used.TryFindByReferenceEquals(hash, type, out used);
-
-                var p = Parent;
-                if (p != null)
-                    return p.TryGetUsed(hash, type, out used);
+                var e = _used.GetEntryOrDefaultByReferenceEquals(hash, type);
+                if (e != null)
+                {
+                    used = e.Value;
+                    return true;
+                }
             }
+            var p = Parent;
+            if (p != null)
+                return p.TryGetUsed(hash, type, out used);
             used = default;
             return false;
         }
