72bcf2065754b09629872754028e791f075fcd9d
dadhi <max.volkov@gmail.com>
2022-08-23 11:59:44 +0200

    added the ifAlreadyRegistered and test project to #520

---
+++ samples/GHIssue520/Program.cs
@@ -0,0 +1,67 @@
+using DryIoc;
+using DryIoc.Microsoft.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+
+var services = new ServiceCollection();
+
+services.AddHttpClient(Options.DefaultName)
+.ConfigurePrimaryHttpMessageHandler(c =>
+{
+    Console.WriteLine("Never called, as it was replaced");
+    return new HttpClientHandler();
+});
+
+services.AddHttpClient<TypedClient>()
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    Console.WriteLine("TypedClient called");
+    return new HttpClientHandler();
+});
+
+Console.WriteLine($"Services ({services.Count}):\n" + string.Join(",\n", services.Select(x => x.ServiceType.Name).ToList()));
+
+var container = new Container(r =>
+    r.WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace))
+.WithDependencyInjectionAdapter(services);
+
+
+var factory = container.Resolve<IHttpClientFactory>();
+Console.WriteLine("Http factory: " + factory.GetType().Name);
+
+using var client = factory.CreateClient();
+Console.WriteLine("Created client: " + client.GetType().Name);
+
+
+public class TypedClient { }
+
+/*
+Program run output:
+
+
+Services (20):
+IOptions`1,
+IOptionsSnapshot`1,
+IOptionsMonitor`1,
+IOptionsFactory`1,
+IOptionsMonitorCache`1,
+ILoggerFactory,
+ILogger`1,
+IConfigureOptions`1,
+HttpMessageHandlerBuilder,
+DefaultHttpClientFactory,
+IHttpClientFactory,
+IHttpMessageHandlerFactory,
+ITypedHttpClientFactory`1,
+Cache,
+IHttpMessageHandlerBuilderFilter,
+HttpClientMappingRegistry,
+HttpClient,
+IConfigureOptions`1,
+TypedClient,
+IConfigureOptions`1
+Http factory: DefaultHttpClientFactory
+Never called, as it was replaced
+Created client: HttpClient
+*/
