02aafbe788a9c7cb54ba36fd92ddaf4b6e37177b
dadhi <max.volkov@gmail.com>
2022-08-25 13:01:24 +0200

    confirming the #522

--- samples/GHIssue520/Program.cs
+++ samples/GHIssue520/Program.cs
@@ -3,29 +3,38 @@ using DryIoc.Microsoft.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System.Net.Http;
+using System.Text.Json;
 
-var services = new ServiceCollection();
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.Configure<TransientFaultHandlingOptions>(
+    builder.Configuration.GetSection(key: nameof(TransientFaultHandlingOptions)));
+
+services.AddSingleton<ExampleService>();
 
 services.AddHttpClient(Options.DefaultName)
-.ConfigurePrimaryHttpMessageHandler(c =>
-{
-    Console.WriteLine("Never called, as it was replaced");
-    return new HttpClientHandler();
-});
+    .ConfigurePrimaryHttpMessageHandler(c =>
+    {
+        Console.WriteLine("Never called, as it was replaced");
+        return new HttpClientHandler();
+    });
 
 services.AddHttpClient<TypedClient>()
-.ConfigurePrimaryHttpMessageHandler(() =>
-{
-    Console.WriteLine("TypedClient called");
-    return new HttpClientHandler();
-});
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        Console.WriteLine("TypedClient called");
+        return new HttpClientHandler();
+    });
 
-Console.WriteLine($"Services ({services.Count}):\n" + string.Join(",\n", services.Select(x => x.ServiceType.Name).ToList()));
+
+Console.WriteLine($"Services ({services.Count}):\n{(string.Join(",\n", services.Select((x, i) => $"{i:000} - {x.ServiceType.Print()}").ToList()))}\n\n");
 
 var container = new Container(r =>
     r.WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace))
 .WithDependencyInjectionAdapter(services);
 
+var app = builder.Build();
 
 var factory = container.Resolve<IHttpClientFactory>();
 Console.WriteLine("Http factory: " + factory.GetType().Name);
@@ -33,8 +42,26 @@ Console.WriteLine("Http factory: " + factory.GetType().Name);
 using var client = factory.CreateClient();
 Console.WriteLine("Created client: " + client.GetType().Name);
 
+container.Resolve<ExampleService>();
 
-public class TypedClient { }
+public class TypedClient {}
+
+public class ExampleService 
+{ 
+    public ExampleService(IOptions<TransientFaultHandlingOptions> options) 
+    {
+        var json = JsonSerializer.Serialize(options.Value);
+        Console.WriteLine($"Options: `{json}`");
+    }
+}
+
+
+
+public class TransientFaultHandlingOptions
+{
+    public bool Enabled { get; set; }
+    public TimeSpan AutoRetryDelay { get; set; }
+}
 
 /*
 Program run output:
