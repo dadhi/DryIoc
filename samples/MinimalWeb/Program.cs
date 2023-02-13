fb943bd73afb5bea3ff64a8484429a4559be5d80
dadhi <max.volkov@gmail.com>
2023-02-13 11:50:16 +0100

    using WithConcreteTypeDynamicRegistrations in MinimalAPI example for #555

--- samples/MinimalWeb/Program.cs
+++ samples/MinimalWeb/Program.cs
@@ -1,9 +1,10 @@
 using DryIoc;
 using DryIoc.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var container = new Container(Rules.MicrosoftDependencyInjectionRules);
+var container = new Container(Rules.MicrosoftDependencyInjectionRules.WithConcreteTypeDynamicRegistrations(reuse: Reuse.Transient));
 
 // register natively with DryIoc
 container.Register<Bar>();
@@ -11,18 +12,25 @@ container.Register<Bar>();
 builder.Host.UseServiceProviderFactory(new DryIocServiceProviderFactory(container));
 
 // register via Services collection
-builder.Services.AddTransient<Foo>();
+builder.Services.AddTransient<IFoo, Foo>();
 
 var app = builder.Build();
 
-app.MapGet("/", (Foo foo) => $"Hello world with `{foo}`");
-app.MapGet("/bar", (Foo foo, Bar bar) => $"Hello world with `{foo}` and `{bar}`");
+app.MapGet("/", (IFoo foo) => $"Hello world with `{foo}`");
+app.MapGet("/bar", (IFoo foo, Bar bar) => $"Hello world with `{foo}` and `{bar}`");
+
+// enabled by adding the `WithConcreteTypeDynamicRegistrations` container rules
+app.MapGet("/pooh", ([FromServices]Pooh p) => $"Hello world with concrete type `{p}`");
 
 app.Run();
 
-public class Foo
+public interface IFoo {}
+
+public class Foo : IFoo
 {
     public Foo(Bar bar = null) {}
 }
 
 public class Bar {}
+
+public class Pooh {}
