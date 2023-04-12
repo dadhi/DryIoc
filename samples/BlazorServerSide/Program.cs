9afe3e0db339cfe65f432071f08a4eb34c183b7a
dadhi <max.volkov@gmail.com>
2023-04-12 20:51:17 +0200

    working example for #567

--- samples/BlazorServerSide/Program.cs
+++ samples/BlazorServerSide/Program.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorServerSide.Data;
+using Serilog;
+using DryIoc;
+using DryIoc.Microsoft.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var container = new Container(
+    // this is causing the #567 error
+    // Rules.MicrosoftDependencyInjectionRules.With(propertiesAndFields: PropertiesAndFields.Auto)
+);
+
+// Here it goes the integration with the existing DryIoc container
+var diFactory = new DryIocServiceProviderFactory(container, RegistrySharing.Share);
+builder.Host.UseServiceProviderFactory(diFactory);
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DetailedErrors = true;
+});
+
+// Configure Serilog Logger
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.File("Errors/Log_.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+builder.Host.UseSerilog(logger);
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
