be36fb098a0a3858b970c13b63bcaa4f80428d8d
dadhi <max.volkov@gmail.com>
2023-04-17 15:09:58 +0200

    added answer to the SO question: "Difficulty using DryIOC in Web form"

---
+++ test/DryIoc.IssuesTests/Samples/DefaultReuseTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace DryIoc.IssuesTests.Samples
+{
+    [TestFixture]
+    public class DefaultReuseTest : ITest
+    {
+        public int Run()
+        {
+            Default_scoped_reuse();
+            return 1;
+        }
+
+        [Test]
+        public void Default_scoped_reuse()
+        {
+            var container = new Container(rules => rules.WithDefaultReuse(Reuse.Scoped));
+
+            container.Register<ScopeService>();
+
+            using (var scope = container.OpenScope())
+            {
+                var oSingleton = scope.Resolve<ScopeService>();
+
+                var oSingleton2 = scope.Resolve<ScopeService>();
+
+                Assert.AreSame(oSingleton, oSingleton2);
+            }
+        }
+
+        class ScopeService
+        {
+        }
+    }
+}
