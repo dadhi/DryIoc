6f8a6dade7ea9f4ae5b9f4fe0c5588ad27672cee
Tom Collings <tom.collings@gmail.com>
2023-05-09 14:57:25 +0100

    Prepare test for GHIssue574

--- test/DryIoc.IssuesTests/GHIssue574_Cannot_register_multiple_impls_in_child_container_with_default_service_key.cs
+++ test/DryIoc.IssuesTests/GHIssue574_Cannot_register_multiple_impls_in_child_container_with_default_service_key.cs
@@ -1,57 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Microsoft.Extensions.DependencyInjection;
+using DryIoc.Microsoft.DependencyInjection;
 
-public class DryExamples
+namespace DryIoc.IssuesTests
 {
-    [Fact] // This works ok.
-    public void TransferMultipleThenResolveEnumerable()
+    public class GHIssue574_Cannot_register_multiple_impls_in_child_container_with_default_service_key
     {
-        var services = new ServiceCollection();
-
-        services.AddScoped<IPrinter, Printer>();
-        services.AddScoped<IPrinter, PrinterA>();
-        services.AddScoped<IPrinter, PrinterB>();
-        services.AddScoped<IPrinter, NeighborPrinter>();
-
-        var spf = new DryIocServiceProviderFactory();
-        var dryContainer = spf.CreateBuilder(services);
-        var msContainer = dryContainer.GetServiceProvider();
-
-        Assert.Equal(
-            dryContainer.Resolve<IEnumerable<IPrinter>>().Count(),
-            msContainer.GetRequiredService<IEnumerable<IPrinter>>().Count());
-    }
-
-    [Fact] // I have not been able to get this to work.
-    public void TransferMultipleThenResolveEnumerableFromChild()
-    {
-        var services = new ServiceCollection();
-
-        services.AddScoped<IPrinter, Printer>();
-        services.AddScoped<IPrinter, PrinterA>();
-        services.AddScoped<IPrinter, PrinterB>();
-        services.AddScoped<IPrinter, NeighborPrinter>();
-
-        var spf = new DryIocServiceProviderFactory();
-        var rootContainer = spf.CreateBuilder(new ServiceCollection());
-        var childContainer = rootContainer.CreateChild(RegistrySharing.Share, "child-stamp", IfAlreadyRegistered.AppendNewImplementation);
-        //childContainer.Populate(services);
-        foreach (var service in services)
+        [Ignore("fixme")]
+        public void ResolveEnumerableFromChild()
         {
-            childContainer.RegisterDescriptor(service, IfAlreadyRegistered.AppendNewImplementation, "child-stamp");
+            var services = new ServiceCollection();
+
+            services.AddScoped<IPrinter, Printer>();
+            services.AddScoped<IPrinter, PrinterA>();
+            services.AddScoped<IPrinter, PrinterB>();
+            services.AddScoped<IPrinter, NeighborPrinter>();
+
+            var spf = new DryIocServiceProviderFactory();
+            var rootContainer = spf.CreateBuilder(new ServiceCollection());
+            var childContainer = rootContainer.CreateChild(RegistrySharing.Share, "child-stamp", IfAlreadyRegistered.AppendNewImplementation);
+
+            //childContainer.Populate(services);
+            foreach (var service in services)
+            {
+                childContainer.RegisterDescriptor(service, IfAlreadyRegistered.AppendNewImplementation, "child-stamp");
+            }
+
+            var msContainer = childContainer.GetServiceProvider();
+
+            Assert.That(
+                childContainer.Resolve<IEnumerable<IPrinter>>().Count(),
+                Is.EqualTo(msContainer.GetRequiredService<IEnumerable<IPrinter>>().Count()));
+
+            Assert.That(
+                msContainer.GetRequiredService<IEnumerable<IPrinter>>().Count(),
+                Is.EqualTo(4));
         }
-        var msContainer = childContainer.GetServiceProvider();
 
-        Assert.Equal(
-            childContainer.Resolve<IEnumerable<IPrinter>>().Count(),
-            msContainer.GetRequiredService<IEnumerable<IPrinter>>().Count());
+        private interface IPrinter { }
+
+        private class Printer : IPrinter { }
+
+        private class PrinterA : IPrinter { }
+
+        private class PrinterB : IPrinter { }
+
+        private class NeighborPrinter : IPrinter { }
     }
-
-    private interface IPrinter{}
-
-    private class Printer : IPrinter{}
-
-    private class PrinterA : IPrinter{}
-
-    private class PrinterB : IPrinter{}
-
-    private class NeighborPrinter : IPrinter{}
 }
