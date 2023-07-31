f1ba495d9f08a572f4f8a42b0a4f04d2b4b28ed5
dadhi <max.volkov@gmail.com>
2023-07-31 14:15:53 +0300

    adding the sample for #583

---
+++ test/DryIoc.IssuesTests/GHIssue583_Scope_is_lost_on_CTRL_C.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DryIoc.IssuesTests
+{
+    // Not an actual test but example of the console app with Console events (!!!) and usage of the custom `StaticScopeContext`
+    public class GHIssue583_Scope_is_lost_on_CTRL_C
+    {
+        public int Run()
+        {
+            Test_AsyncExecutionFlowScopeContext().GetAwaiter().GetResult();
+            return 1;
+        }
+
+        public async Task Test_AsyncExecutionFlowScopeContext()
+        {
+            IContainer container = new Container(scopeContext: new StaticScopeContext());
+
+            CancellationTokenSource cts = new();
+
+            using (IResolverContext scope = container.OpenScope("MyScope", trackInParent: true))
+            {
+                Console.CancelKeyPress += (sender, args) =>
+                {
+                    Console.WriteLine("Canceling");
+                    Console.WriteLine(scope.CurrentScope?.ToString() ?? "No scope");
+
+                    cts.Cancel();
+                    args.Cancel = true;
+                };
+
+                while (!cts.IsCancellationRequested)
+                {
+                    Console.WriteLine(scope.CurrentScope.ToString());
+
+                    await Task.Delay(1000).ConfigureAwait(false);
+                }
+            }
+
+            Console.WriteLine("Scope disposed");
+        }
+    }
+
+    internal sealed class StaticScopeContext : IScopeContext
+    {
+        private static IScope scope;
+
+        public IScope GetCurrentOrDefault() => scope;
+
+        public IScope SetCurrent(SetCurrentScopeHandler changeCurrentScope) => scope = changeCurrentScope(GetCurrentOrDefault());
+
+        public void Dispose()
+        {
+        }
+    }
+}
