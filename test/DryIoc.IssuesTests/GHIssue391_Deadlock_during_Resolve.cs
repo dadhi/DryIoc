1803ffe16346056d0ae39b2c7e8a3959c1b85e3e
dadhi <max.volkov@gmail.com>
2023-10-18 10:26:39 +0200

    adding missing test

--- test/DryIoc.IssuesTests/GHIssue391_Deadlock_during_Resolve.cs
+++ test/DryIoc.IssuesTests/GHIssue391_Deadlock_during_Resolve.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System.Linq;
 
 namespace DryIoc.IssuesTests
 {
@@ -8,12 +7,14 @@ namespace DryIoc.IssuesTests
     {
         public int Run()
         {
-            Test1();
-            return 1;
+            Test_non_generic_with_Decorator(); // actually related to the #598
+            Test_non_generic();
+            Test_open_generic();
+            return 3;
         }
 
         [Test]
-        public void Test1()
+        public void Test_non_generic()
         {
             var container = new Container(rules => rules
                 .With(FactoryMethod.ConstructorWithResolvableArguments)
@@ -22,13 +23,13 @@ namespace DryIoc.IssuesTests
                 .WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace));
 
             container.Register(typeof(A), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
-            container.RegisterMany(new [] { typeof(A), typeof(IA) }, typeof(A), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(A), typeof(IA) }, typeof(A), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
 
             container.Register(typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
-            container.RegisterMany(new [] { typeof(B), typeof(IB) }, typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(B), typeof(IB) }, typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
 
             container.Register(typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
-            container.RegisterMany(new [] { typeof(C), typeof(IC) }, typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(C), typeof(IC) }, typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
 
             // the missing dependency
             // container.Register<ID, D>(Reuse.Singleton);
@@ -42,19 +43,107 @@ namespace DryIoc.IssuesTests
             Assert.AreSame(Error.NameOf(Error.WaitForScopedServiceIsCreatedTimeoutExpired), ex.ErrorName);
 
             var m = ex.TryGetDetails(container);
+            StringAssert.Contains("A`", m);
+        }
+
+        [Test]
+        public void Test_non_generic_with_Decorator()
+        {
+            var container = new Container(rules => rules
+                .With(FactoryMethod.ConstructorWithResolvableArguments)
+                .WithoutEagerCachingSingletonForFasterAccess()
+                .WithoutThrowOnRegisteringDisposableTransient()
+                .WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace));
+
+            container.Register<IA, AD>(Reuse.Singleton, setup: Setup.Decorator);
+
+            container.Register(typeof(A), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(A), typeof(IA) }, typeof(A), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+
+            container.Register(typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(B), typeof(IB) }, typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+
+            container.Register(typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(C), typeof(IC) }, typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+
+            // the missing dependency
+            // container.Register<ID, D>(Reuse.Singleton);
+
+            // A -> B -> C -> D(missing)
+            //   \----->
+
+            Assert.Throws<ContainerException>(() => container.Resolve<IA>());
+
+            var ex = Assert.Throws<ContainerException>(() => container.Resolve<IA>());
+            Assert.AreSame(Error.NameOf(Error.WaitForScopedServiceIsCreatedTimeoutExpired), ex.ErrorName);
+
+            var m = ex.TryGetDetails(container);
+            StringAssert.Contains("DecoratorType=", m);
+            StringAssert.Contains("ServiceType=", m);
             StringAssert.Contains("IA`", m);
         }
 
-        public interface IA {}
-        public interface IB {}
-        public interface IC {}
-        public interface ID {}
+        [Test]
+        public void Test_open_generic()
+        {
+            var container = new Container(rules => rules
+                .With(FactoryMethod.ConstructorWithResolvableArguments)
+                .WithoutEagerCachingSingletonForFasterAccess()
+                .WithoutThrowOnRegisteringDisposableTransient()
+                .WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace));
+
+            container.Register(typeof(A<>), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(A<>), typeof(IA<>) }, typeof(A<>), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+
+            container.Register(typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(B), typeof(IB) }, typeof(B), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+
+            container.Register(typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+            container.RegisterMany(new[] { typeof(C), typeof(IC) }, typeof(C), Reuse.Singleton, setup: Setup.With(asResolutionCall: true));
+
+            // the missing dependency
+            // container.Register<ID, D>(Reuse.Singleton);
+
+            // A -> B -> C -> D(missing)
+            //   \----->
+
+            Assert.Throws<ContainerException>(() => container.Resolve<IA<B>>());
+
+            var ex = Assert.Throws<ContainerException>(() => container.Resolve<IA<B>>());
+            Assert.AreSame(Error.NameOf(Error.WaitForScopedServiceIsCreatedTimeoutExpired), ex.ErrorName);
+
+            var m = ex.TryGetDetails(container);
+            StringAssert.Contains("A<>", m);
+        }
+
+        public interface IA { }
+        public interface IA<TB> { }
+        public interface IB { }
+        public interface IC { }
+        public interface ID { }
 
         public class A : IA
         {
-            private IB B;
-            private IC C;
-            public A(IC c,IB b)
+            public IB B;
+            public IC C;
+            public A(IC c, IB b)
+            {
+                B = b;
+                C = c;
+            }
+        }
+
+        public class AD : IA
+        {
+            public IA A;
+            public AD(IA a) => A = a;
+        }
+
+        public class A<TB> : IA<TB>
+        {
+            public TB B;
+            public IC C;
+            public A(IC c, TB b)
             {
                 B = b;
                 C = c;
@@ -63,16 +152,16 @@ namespace DryIoc.IssuesTests
 
         public class B : IB
         {
-            private IC C;
+            public IC C;
             public B(IC c) => C = c;
         }
 
         public class C : IC
         {
-            private ID D;
+            public ID D;
             public C(ID d) => D = d;
         }
 
-        public class D : ID {}
+        public class D : ID { }
     }
 }
