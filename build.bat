c383853cc7113474dd451b2d4413bfd74a421185
dadhi <max.volkov@gmail.com>
2023-11-14 16:19:19 +0100

    updating deps for the MS.DI

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-dotnet clean -v:m
-dotnet build -c:Release -v:m
+dotnet clean -v:m -p:DevMode=false
+dotnet build -c:Release -v:m -p:DevMode=false
 if %ERRORLEVEL% neq 0 goto :error
 
 echo:
@@ -11,7 +11,7 @@ echo:
 echo:## Starting: TestRunner...
 echo:
 
-dotnet run --no-build -c Release --project test/DryIoc.TestRunner/DryIoc.TestRunner.csproj
+dotnet run --no-build -f net7.0 -c Release -p:DevMode=false --project test/DryIoc.TestRunner/DryIoc.TestRunner.csproj
 if %ERRORLEVEL% neq 0 goto :error
 dotnet run --no-build -c Release --project test/DryIoc.TestRunner.net472/DryIoc.TestRunner.net472.csproj
 if %ERRORLEVEL% neq 0 goto :error
@@ -22,8 +22,7 @@ echo:
 echo:## Starting: TESTS...
 echo: 
 
-dotnet test --no-build -c:Release
-
+dotnet test --no-build -c:Release -p:DevMode=false
 
 if %ERRORLEVEL% neq 0 goto :error
 
@@ -33,7 +32,7 @@ echo:
 echo:## Starting: DOCUMENTATION GENERATION ##
 echo:
 
-dotnet build docs\DryIoc.Docs\DryIoc.Docs.csproj -f net6.0 -target:MdGenerate
+dotnet build docs\DryIoc.Docs\DryIoc.Docs.csproj -f net6.0 -target:MdGenerate -p:DevMode=false
 
 echo:
 echo:## Finished: DOCUMENTATION GENERATION ##
