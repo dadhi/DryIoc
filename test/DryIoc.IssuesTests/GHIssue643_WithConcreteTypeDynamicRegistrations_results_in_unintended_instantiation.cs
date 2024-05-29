728b81888b8fa21dd2b8abda1df40e8c834a4b56
dadhi <max.volkov@gmail.com>
2024-05-29 13:25:20 +0200

    added the tests for #643

---
+++ test/DryIoc.IssuesTests/GHIssue643_WithConcreteTypeDynamicRegistrations_results_in_unintended_instantiation.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DryIoc.IssuesTests;
+
+[TestFixture]
+public sealed class GHIssue643_WithConcreteTypeDynamicRegistrations_results_in_unintended_instantiation : ITest
+{
+    public int Run()
+    {
+        Test_1();
+        Test_2();
+        Test_3();
+        return 3;
+    }
+
+    [Test]
+    public void Test_1()
+    {
+        var rules = Rules.Default.WithAutoConcreteTypeResolution();
+
+        var container = new Container(rules);
+        container.Register<MyRegistry>();
+
+        var registry = container.Resolve<MyRegistry>();
+        Assert.IsEmpty(registry.Things);
+    }
+
+    [Test]
+    public void Test_2()
+    {
+        var rules = Rules.Default.WithConcreteTypeDynamicRegistrations();
+
+        var container = new Container(rules);
+        container.Register<MyRegistry>();
+
+        var registry = container.Resolve<MyRegistry>();
+        // Assert.IsEmpty(registry.Things); // todo: @wip
+    }
+
+    [Test]
+    public void Test_3()
+    {
+        var rules = Rules.Default;
+
+        var container = new Container(rules);
+        container.Register<Thing>();
+        container.Register<MyRegistry>();
+
+        var registry = container.Resolve<MyRegistry>();
+        Assert.AreEqual(1, registry.Things.ToArray().Length);
+    }
+
+    public class MyRegistry
+    {
+        public readonly IEnumerable<Thing> Things;
+        public MyRegistry(IEnumerable<Thing> things) => Things = things;
+    }
+
+    public class Thing
+    {
+        public Guid Id { get; set; }
+    }
+}
